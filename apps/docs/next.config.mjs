271b5529d7f50a8aadcf8ec5620d12b62157bfd1
Ian Lovell <ian.lovell@nearform.com>
2024-05-29 12:15:58 +0100

    wip: update

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,7 +1,7 @@
 import { withContentlayer } from "next-contentlayer";
 
 const isCI = Boolean(process.env.CI);
-const prefix = isCI ? "/govie-ds" : undefined;
+const prefix = undefined;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
