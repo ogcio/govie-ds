48ab8a99bca2c69879286c2ca2c038cae650c904
Ian Lovell <ian.lovell@nearform.com>
2024-06-03 15:10:14 +0100

    wip: update

--- apps/docs/src/components/chrome/top-bar.tsx
+++ apps/docs/src/components/chrome/top-bar.tsx
@@ -1,15 +1,21 @@
-"use client";
-import { useState } from "react";
-import { Header } from "./header";
-import { MainMenuConnected } from "../navigation/main-menu-connected";
-import { MobileMenuConnected } from "../navigation/mobile-menu-connected";
+'use client';
+import { useState } from 'react';
+import { Header } from './header';
+import { MainMenuConnected } from '../navigation/main-menu-connected';
+import { MobileMenuConnected } from '../navigation/mobile-menu-connected';
+import { usePathname } from 'next/navigation';
 
 export function TopBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
+  // TODO: review showing main menu on the home page
   return (
     <div>
-      <Header onMobileMenuSelect={() => setIsOpen((isOpen) => !isOpen)} />
+      <Header
+        showMobileMenu={pathname === '/' ? false : true}
+        onMobileMenuSelect={() => setIsOpen((isOpen) => !isOpen)}
+      />
       <MainMenuConnected />
       <MobileMenuConnected
         isOpen={isOpen}
