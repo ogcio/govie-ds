679ad568253087dc898196ef5eb05473d8d6774c
Ian Lovell <ian.lovell@nearform.com>
2024-06-03 10:19:29 +0100

    wip: update

--- packages/themes/govie/src/index.ts
+++ packages/themes/govie/src/index.ts
@@ -1,9 +1,9 @@
-import { metaLight } from "./dist/meta-light.js"; // TODO: auto-generate this file
-import { metaLightUnresolved } from "./dist/meta-light-unresolved.js";
-import { metaDark } from "./dist/meta-dark.js";
-import { metaDarkUnresolved } from "./dist/meta-dark-unresolved.js";
-import * as tokensLight from "./dist/tokens-light.js";
-import * as tokensDark from "./dist/tokens-dark.js";
+import { metaLight } from './dist/meta-light.js'; // TODO: auto-generate this file
+import { metaLightUnresolved } from './dist/meta-light-unresolved.js';
+import { metaDark } from './dist/meta-dark.js';
+import { metaDarkUnresolved } from './dist/meta-dark-unresolved.js';
+import * as tokensLight from './dist/tokens-light.js';
+import * as tokensDark from './dist/tokens-dark.js';
 
 export const meta = {
   light: {
