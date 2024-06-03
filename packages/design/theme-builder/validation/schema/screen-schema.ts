8c02e71e28cfbcad5c7c7b577cee089123010634
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2024-06-03 12:44:31 +0100

    fix: rename breakpoints (#29)

---
+++ packages/design/theme-builder/validation/schema/screen-schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import { createPixelSchema, createTokenSchema } from "./shared.js";
+
+function createScreenSchema(name: string) {
+  return createTokenSchema({
+    type: "dimension",
+    valueSchema: createPixelSchema("Screen"),
+    name,
+  });
+}
+
+export const screenSchema = z
+  .object({
+    xs: createScreenSchema("xs"),
+    sm: createScreenSchema("sm"),
+    md: createScreenSchema("md"),
+    lg: createScreenSchema("lg"),
+    xl: createScreenSchema("xl"),
+    "2xl": createScreenSchema("2xl"),
+  })
+  .strict();
