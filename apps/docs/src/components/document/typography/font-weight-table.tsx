5848ca55260eb31dc8c186efe8a8524f93d1dbe0
Ian Lovell <ian.lovell@nearform.com>
2024-06-04 16:06:13 +0100

    wip: update table

--- apps/docs/src/components/document/typography/font-weight-table.tsx
+++ apps/docs/src/components/document/typography/font-weight-table.tsx
@@ -3,9 +3,9 @@ import { FontTable } from './font-table';
 
 export function FontWeightTable() {
   return (
-    <FontTable<string>
+    <FontTable<number>
       name="font-weight"
-      tokens={meta.light.resolved.primitive.font.size}
+      tokens={meta.light.resolved.primitive.font.weight}
       renderValue={(value) => value}
       renderExample={(value) => (
         <span style={{ fontWeight: value }}>Sample text</span>
