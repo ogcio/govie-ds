51fbf45798b6052a6d60bf94b7c9f0929ddc7dbe
Ian Lovell <ian.lovell@nearform.com>
2024-06-04 16:09:17 +0100

    wip: update

--- apps/docs/src/components/document/typography/letter-spacing-table.tsx
+++ apps/docs/src/components/document/typography/letter-spacing-table.tsx
@@ -1,14 +1,18 @@
 import { meta } from '@govie-ds/theme-govie';
 import { FontTable } from './font-table';
 
+function remToEm(value: string) {
+  return value.replace('rem', 'em');
+}
+
 export function LetterSpacingTable() {
   return (
     <FontTable<string>
       name="letter-spacing"
       tokens={meta.light.resolved.primitive.font.letterSpacing}
-      renderValue={(value) => value}
+      renderValue={(value) => remToEm(value)}
       renderExample={(value) => (
-        <span style={{ letterSpacing: value }}>Sample text</span>
+        <span style={{ letterSpacing: remToEm(value) }}>Sample text</span>
       )}
     />
   );
