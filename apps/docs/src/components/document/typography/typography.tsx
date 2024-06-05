55abb292606177f34cf22e204f5fd96679721fb1
Ian Lovell <ian.lovell@nearform.com>
2024-06-05 11:16:34 +0100

    wip: update scale

--- apps/docs/src/components/document/typography/typography.tsx
+++ apps/docs/src/components/document/typography/typography.tsx
@@ -1,8 +1,5 @@
 import { meta } from '@govie-ds/tokens';
-import { objectKeys } from 'ts-extras';
-import { Table, Td } from './table';
-import { TokenName } from '../color/token-name';
-import { P } from 'vitest/dist/reporters-yx5ZTtEV.js';
+import { Td } from './table';
 import { FontTable } from './font-table';
 import { Heading } from '@/components/typography/heading';
 
@@ -10,7 +7,14 @@ function remToPx(remString: string) {
   return `${Number(remString.replace('rem', '')) * 16}px`;
 }
 
-function unitlessToPx(unitless: number) {
+function lineHeightToPx({
+  fontSize,
+  lineHeight,
+}: {
+  fontSize: string;
+  lineHeight: number;
+}) {
+  const unitless = Number(fontSize.replace('rem', '')) * lineHeight;
   return `${unitless * 16}px`;
 }
 
@@ -30,22 +34,20 @@ function TypographyCell({
   lineHeight: number;
 }) {
   return (
-    <Td className="text-sm">
-      <div className="grid grid-cols-[auto,1fr] gap-x-md">
-        <CellLabel label="Font family" />
-        <p>{fontFamily.join(', ')}</p>
-        <CellLabel label="Font size" />
-        <p>
-          {fontSize} ({remToPx(fontSize)})
-        </p>
-        <CellLabel label="Font weight" />
-        <p>{fontWeight}</p>
-        <CellLabel label="Line height" />
-        <p>
-          {lineHeight} ({unitlessToPx(lineHeight)})
-        </p>
-      </div>
-    </Td>
+    <div className="grid grid-cols-[auto,1fr] gap-x-md">
+      <CellLabel label="Font family" />
+      <p>{fontFamily.join(', ')}</p>
+      <CellLabel label="Font size" />
+      <p>
+        {fontSize} ({remToPx(fontSize)})
+      </p>
+      <CellLabel label="Font weight" />
+      <p>{fontWeight}</p>
+      <CellLabel label="Line height" />
+      <p>
+        {lineHeight} ({lineHeightToPx({ fontSize, lineHeight })})
+      </p>
+    </div>
   );
 }
 
@@ -89,31 +91,17 @@ export function Typography() {
   return (
     <div className="flex flex-col gap-2xl">
       <div>
-        <Heading as="h3">Heading (Regular)</Heading>
+        <Heading as="h3">Heading</Heading>
         <TypographyTable
           name="heading/regular"
           tokens={meta.light.resolved.primitive.heading.regular}
         />
       </div>
       <div>
-        <Heading as="h3">Heading (Bold)</Heading>
-        <TypographyTable
-          name="heading/bold"
-          tokens={meta.light.resolved.primitive.heading.bold}
-        />
-      </div>
-      <div>
-        <Heading as="h3">Text (Regular)</Heading>
+        <Heading as="h3">Text</Heading>
         <TypographyTable
           name="text/regular"
           tokens={meta.light.resolved.primitive.text.regular}
-        />
-      </div>
-      <div>
-        <Heading as="h3">Text (Bold)</Heading>
-        <TypographyTable
-          name="text/bold"
-          tokens={meta.light.resolved.primitive.text.bold}
         />
       </div>
     </div>
