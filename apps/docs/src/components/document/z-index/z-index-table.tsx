529fc546e1ba9397ede9cde65fafc0d6b2bd56af
Ian Lovell <ian.lovell@nearform.com>
2024-06-06 12:24:48 +0100

    wip: update

--- apps/docs/src/components/document/z-index/z-index-table.tsx
+++ apps/docs/src/components/document/z-index/z-index-table.tsx
@@ -13,20 +13,29 @@ export function ZIndexTable() {
       }}
       renderExample={(value) => {
         const items = toSampleTokens(meta.light.resolved.primitive.zIndex);
+        const valueIndex = items.findIndex((item) => item.value === value);
+
+        const itemHeight = 32;
+        const offset = 4;
 
         return (
-          <div className="relative">
+          <div
+            className="relative"
+            style={{ height: itemHeight + (items.length - 1) * offset }}
+          >
             {items.map((item, index) => {
               return (
                 <div
                   className={cn(
-                    'absolute w-[200px] h-[32px]',
-                    item.value === value ? 'bg-gold-200' : 'bg-gray-50',
+                    'absolute w-[200px] border-gray-50 border-xs',
+                    index === valueIndex ? 'bg-gold-200' : 'bg-gray-50',
+                    index > valueIndex ? 'opacity-40' : 'opacity-100',
                   )}
                   style={{
+                    height: itemHeight,
                     zIndex: item.value,
-                    top: `${index * 4}px`,
-                    left: `${index * 4}px`,
+                    top: `${index * offset}px`,
+                    left: `${index * offset}px`,
                   }}
                 />
               );
