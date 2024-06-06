87db904da07e9874d25a2363a612f26299ab582a
Ian Lovell <ian.lovell@nearform.com>
2024-06-06 12:40:13 +0100

    wip: update

--- apps/docs/src/components/document/z-index/z-index-table.tsx
+++ apps/docs/src/components/document/z-index/z-index-table.tsx
@@ -15,31 +15,38 @@ export function ZIndexTable() {
         const items = toSampleTokens(meta.light.resolved.primitive.zIndex);
         const valueIndex = items.findIndex((item) => item.value === value);
 
+        const itemWidth = 200;
         const itemHeight = 32;
         const offset = 4;
 
         return (
-          <div
-            className="relative"
-            style={{ height: itemHeight + (items.length - 1) * offset }}
-          >
-            {items.map((item, index) => {
-              return (
-                <div
-                  className={cn(
-                    'absolute w-[200px] border-gray-50 border-xs',
-                    index === valueIndex ? 'bg-gold-200' : 'bg-gray-50',
-                    index > valueIndex ? 'opacity-40' : 'opacity-100',
-                  )}
-                  style={{
-                    height: itemHeight,
-                    zIndex: item.value,
-                    top: `${index * offset}px`,
-                    left: `${index * offset}px`,
-                  }}
-                />
-              );
-            })}
+          <div>
+            <div
+              className="relative"
+              style={{
+                width: itemWidth + (items.length - 1) * offset,
+                height: itemHeight + (items.length - 1) * offset,
+              }}
+            >
+              {items.map((item, index) => {
+                return (
+                  <div
+                    className={cn(
+                      'absolute border-gray-50 border-xs',
+                      index === valueIndex ? 'bg-gold-200' : 'bg-gray-50',
+                      index > valueIndex ? 'opacity-40' : 'opacity-100',
+                    )}
+                    style={{
+                      width: itemWidth,
+                      height: itemHeight,
+                      zIndex: item.value,
+                      top: `${index * offset}px`,
+                      left: `${index * offset}px`,
+                    }}
+                  />
+                );
+              })}
+            </div>
           </div>
         );
       }}
