30023f03ecf3464742796a64e7ef311c4835e83e
Ian Lovell <ian.lovell@nearform.com>
2024-06-06 08:56:57 +0100

    wip: update

--- apps/docs/src/components/document/common/sample-list.tsx
+++ apps/docs/src/components/document/common/sample-list.tsx
@@ -23,14 +23,16 @@ export function SampleList<TValue>({
             key={key}
             className="grid grid-cols-1 lg:grid-cols-2 gap-2xl border-y-xs border-gray-50 py-xl"
           >
-            <div className="flex flex-col gap-md">
+            <div className="flex flex-col gap-lg">
               <div className="flex">
                 <TokenName name={`${name}/${key}`} />
               </div>
-              <div className="text-gray-900 text-xs">{renderValue(value)}</div>
+              <div className="text-gray-900 text-xs px-md">
+                {renderValue(value)}
+              </div>
             </div>
             <div className="flex flex-col gap-md">
-              <p className="text-gray-600 text-xs font-light">Sample</p>
+              <p className="text-gray-600 text-2xs font-light">Sample</p>
               <div className="flex justify-center sm:justify-start text-center sm:text-start">
                 {renderExample(value)}
               </div>
