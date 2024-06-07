690c6f0a3b55270302056ced54aa77aeeee4ceba
Ian Lovell <ian.lovell@nearform.com>
2024-06-07 12:25:06 +0100

    wip: update

--- apps/docs/src/components/document/typography/responsive.tsx
+++ apps/docs/src/components/document/typography/responsive.tsx
@@ -1,14 +1,25 @@
 'use client';
-import { useState, useEffect, Fragment } from 'react';
+import {
+  useState,
+  useEffect,
+  Fragment,
+  MouseEventHandler,
+  MouseEvent,
+} from 'react';
 import { createPortal } from 'react-dom';
+import { Resizable } from 're-resizable';
 
-function ResponsiveInternal({
+function IFrame({
   title,
+  width,
+  height,
   styleSelector,
   children,
 }: {
   title: string;
-  styleSelector: string;
+  width?: string | number;
+  height?: string | number;
+  styleSelector?: string;
   children: React.ReactNode;
 }) {
   const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
@@ -18,10 +29,15 @@ function ResponsiveInternal({
     if (!contentRef) {
       return;
     }
+
     const window = contentRef?.contentWindow;
 
     if (!window) {
       throw new Error('contentWindow is not available.');
+    }
+
+    if (!styleSelector) {
+      return;
     }
 
     const linkEls = window.parent.document.querySelectorAll(styleSelector);
@@ -34,9 +50,117 @@ function ResponsiveInternal({
   }, [contentRef, styleSelector]);
 
   return (
-    <iframe title={title} ref={setContentRef}>
+    <iframe
+      title={title}
+      width={typeof width === 'number' ? `${width}px` : width}
+      height={typeof height === 'number' ? `${height}px` : height}
+      ref={setContentRef}
+    >
       {mountNode && createPortal(children, mountNode)}
     </iframe>
+  );
+}
+
+// function ResizableIFrame({
+//   title,
+//   width,
+//   height,
+//   styleSelector,
+//   children,
+// }: {
+//   title: string;
+//   width?: string | number;
+//   height?: string | number;
+//   styleSelector?: string;
+//   children: React.ReactNode;
+// }) {
+//   const [currentWidth, setCurrentWidth] = useState<string | number>(
+//     width ?? '100%',
+//   );
+//   const [isDragging, setIsDragging] = useState(false);
+
+//   const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+//     e.preventDefault();
+//     setIsDragging(true);
+//   };
+
+//   const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+//     e.preventDefault();
+//     setIsDragging(false);
+//   };
+
+//   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+//     e.preventDefault();
+
+//     if (isDragging) {
+//       setCurrentWidth(e.clientX);
+//     }
+//   };
+
+//   return (
+//     <Fragment>
+//       {currentWidth}
+//       <p>dragging: {isDragging.toString()}</p>
+
+//       <div
+//         className="relative flex items-stretch"
+//         onMouseMove={handleMouseMove}
+//         onMouseUp={handleMouseUp}
+//       >
+//         <IFrame
+//           title={title}
+//           width={currentWidth}
+//           height={height}
+//           styleSelector={styleSelector}
+//         >
+//           {children}
+//         </IFrame>
+//         <div
+//           className="absolute w-[20px] top-[50%] right-0 cursor-pointer"
+//           onMouseDown={handleMouseDown}
+//           onMouseUp={handleMouseUp}
+//         >
+//           |
+//         </div>
+//       </div>
+//     </Fragment>
+//   );
+// }
+
+function ResizableIFrame({
+  title,
+  width,
+  height,
+  styleSelector,
+  children,
+}: {
+  title: string;
+  width?: string | number;
+  height?: string | number;
+  styleSelector?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Resizable
+      style={{
+        border: '1px solid #eee',
+        borderRadius: '4px',
+      }}
+      minWidth={200}
+      maxWidth={width}
+      minHeight={height}
+      maxHeight={height}
+      bounds="parent"
+    >
+      <IFrame
+        title={title}
+        width="100%"
+        height="100%"
+        styleSelector={styleSelector}
+      >
+        {children}
+      </IFrame>
+    </Resizable>
   );
 }
 
@@ -50,9 +174,13 @@ export function Responsive({
   return (
     <Fragment>
       <link data-frame type="text/css" rel="stylesheet" href="/styles.css" />
-      <ResponsiveInternal title={title} styleSelector="link[data-frame]">
+      <ResizableIFrame
+        title={title}
+        height={200}
+        styleSelector="link[data-frame]"
+      >
         {children}
-      </ResponsiveInternal>
+      </ResizableIFrame>
     </Fragment>
   );
 }
