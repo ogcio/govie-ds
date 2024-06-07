a0fec1bd6917f22dcdad8810aef0d0f066c03d4d
Ian Lovell <ian.lovell@nearform.com>
2024-06-07 10:35:57 +0100

    wip: update

--- apps/docs/src/components/document/typography/responsive.tsx
+++ apps/docs/src/components/document/typography/responsive.tsx
@@ -1,13 +1,40 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export function Responsive({ children }: { children: React.ReactNode }) {
+export function Responsive({
+  title,
+  styleSelector,
+  children,
+}: {
+  title: string;
+  styleSelector: string;
+  children: React.ReactNode;
+}) {
   const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
-  const mountNode = contentRef?.contentWindow?.document?.body;
+  const mountNode = contentRef?.contentWindow?.document.body;
+
+  useEffect(() => {
+    if (!contentRef) {
+      return;
+    }
+    const window = contentRef?.contentWindow;
+
+    if (!window) {
+      throw new Error('contentWindow is not available.');
+    }
+
+    const linkEls = window.parent.document.querySelectorAll(styleSelector);
+
+    if (linkEls.length) {
+      linkEls.forEach((el) => {
+        window.document.head.appendChild(el);
+      });
+    }
+  }, [contentRef, styleSelector]);
 
   return (
-    <iframe ref={setContentRef}>
+    <iframe title={title} ref={setContentRef}>
       {mountNode && createPortal(children, mountNode)}
     </iframe>
   );
--- apps/docs/src/components/document/typography/typography-responsive.tsx
+++ apps/docs/src/components/document/typography/typography-responsive.tsx
@@ -1,9 +1,18 @@
+import { Fragment } from 'react';
 import { Responsive } from './responsive';
 
 export function TypographyResponsive() {
   return (
-    <Responsive>
-      <h1 className="bg-gold-200">Hello world</h1>
-    </Responsive>
+    <Fragment>
+      <link
+        data-frame
+        type="text/css"
+        rel="stylesheet"
+        href="/_next/static/css/app/layout.css"
+      />
+      <Responsive title="Sample" styleSelector="link[data-frame]">
+        <h1 className="bg-gold-200">Hello world</h1>
+      </Responsive>
+    </Fragment>
   );
 }
