0bd382a8e48f3c351408b5483697b6f7eed45aca
Ian Lovell <ian.lovell@nearform.com>
2024-06-11 13:47:29 +0100

    wip: update

--- apps/docs/src/components/document/typography/typography-responsive-sizes.tsx
+++ apps/docs/src/components/document/typography/typography-responsive-sizes.tsx
@@ -1,34 +1,62 @@
 import { meta } from '@govie-ds/tokens';
 import { objectKeys } from 'ts-extras';
 
+type TypographyScreenSize = {
+  size: string;
+  value: string;
+};
+
+type TypographySize = {
+  token: string;
+  sizes: TypographyScreenSize[];
+};
+
 export function TypographyResponsiveSizes() {
   const screenSizes = ['xs', 'sm', 'md', 'lg', 'xl'] as const;
 
-  const typographyTokens = screenSizes.map((screenSize) => {
-    const resolved = Object.hasOwn(
-      meta.light.resolved.semantic.typography,
-      screenSize,
-    )
-      ? meta.light.resolved.semantic.typography[
-          screenSize as keyof typeof meta.light.resolved.semantic.typography
-        ]
-      : meta.light.resolved.semantic.typography.default;
+  const foo: TypographySize[] = objectKeys(
+    meta.light.resolved.semantic.typography,
+  )
+    .filter((size) => size !== 'default')
+    .map((size) => {
+      return {
+        token: key,
+        sizes: [
+          {
+            size: 'xs',
+            value: 'foo',
+          },
+        ],
+      };
+    });
 
-    const unresolved = Object.hasOwn(
-      meta.light.unresolved.semantic.typography,
-      screenSize,
-    )
-      ? meta.light.unresolved.semantic.typography[
-          screenSize as keyof typeof meta.light.unresolved.semantic.typography
-        ]
-      : meta.light.unresolved.semantic.typography.default;
+  console.log({ foo });
 
-    return {
-      screenSize,
-      resolved,
-      unresolved,
-    };
-  });
+  // const typographyTokens = screenSizes.map((screenSize) => {
+  //   const resolved = Object.hasOwn(
+  //     meta.light.resolved.semantic.typography,
+  //     screenSize,
+  //   )
+  //     ? meta.light.resolved.semantic.typography[
+  //         screenSize as keyof typeof meta.light.resolved.semantic.typography
+  //       ]
+  //     : meta.light.resolved.semantic.typography.default;
+
+  //   const unresolved = Object.hasOwn(
+  //     meta.light.unresolved.semantic.typography,
+  //     screenSize,
+  //   )
+  //     ? meta.light.unresolved.semantic.typography[
+  //         screenSize as keyof typeof meta.light.unresolved.semantic.typography
+  //       ]
+  //     : meta.light.unresolved.semantic.typography.default;
+
+  //   return {
+  //     screenSize,
+  //     resolved,
+  //     unresolved,
+  //   };
+  // });
 
   return (
     <div>
@@ -36,7 +64,7 @@ export function TypographyResponsiveSizes() {
         <thead>
           <th></th>
           {screenSizes.map((size) => (
-            <th>screen/{size}</th>
+            <th key={size}>screen/{size}</th>
           ))}
         </thead>
         <tbody></tbody>
