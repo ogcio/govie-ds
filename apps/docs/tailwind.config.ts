9fb3c51540cf88d1c49a5b0f7d35863eeee92a82
Ian Lovell <ian.lovell@nearform.com>
2024-06-20 14:37:18 +0100

    wip: add tailwind

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-import { createTheme } from '@govie-ds/design-components';
+import { createTheme } from '@govie-ds/tailwind';
 
 const useVariables = !(process.env.USE_RESOLVED_THEME === 'true');
 
@@ -8,7 +8,6 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@govie-ds/design-components/src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: createTheme({
     useVariables,
--- packages/design/components/src/token/token-name.tsx
+++
@@ -1,7 +0,0 @@
-export function TokenName({ name }: { name: string }) {
-  return (
-    <div className="rounded-md bg-gray-100 shadow-sm px-sm py-xs text-center text-sm text-gray-600 font-medium">
-      {name}
-    </div>
-  );
-}
