32ee4d2d39a41e5df1d1044613afe4393d3e37da
Ian Lovell <ian.lovell@nearform.com>
2024-07-01 21:20:27 +0100

    wip: add filled

--- packages/react/ds/src/icon/svg.tsx
+++ packages/react/ds/src/icon/svg.tsx
@@ -2,10 +2,12 @@ export function Svg({
   width,
   height,
   children,
+  filled = false,
 }: {
   width: number | string;
   height: number | string;
   children: React.ReactNode;
+  filled?: boolean;
 }) {
   return (
     <svg
@@ -13,8 +15,8 @@ export function Svg({
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
+      fill={filled ? 'currentColor' : 'none'}
+      stroke={filled ? 'none' : 'currentColor'}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
