5b787ae4faeaeffb218a708b45572ab110c59759
Ian Lovell <ian.lovell@nearform.com>
2024-07-18 11:49:32 +0100

    wip: update

--- apps/docs/src/components/document/typography/font-family-table.tsx
+++ apps/docs/src/components/document/typography/font-family-table.tsx
@@ -4,16 +4,30 @@ import { sampleTextShort } from '../common/sample-text';
 import { TokenValue } from '../common/token-value';
 
 export function FontFamilyTable() {
+  // return (
+  //   <SampleList<string[]>
+  //     name="font-family"
+  //     tokens={toSampleTokens(meta.light.resolved.primitive.font.family)}
+  //     renderValue={(value) => {
+  //       return <TokenValue value={value.join(', ')} />;
+  //     }}
+  //     renderExample={(value) => (
+  //       <span style={{ fontFamily: value.join(', ') }}>{sampleTextShort}</span>
+  //     )}
+  //   />
+  // );
+
+  return <SampleTable />;
+}
+
+function SampleTable({ tokenName = 'Token' }: { tokenName?: string }) {
+  const headers = [tokenName, 'Value', 'Sample'];
+
   return (
-    <SampleList<string[]>
-      name="font-family"
-      tokens={toSampleTokens(meta.light.resolved.primitive.font.family)}
-      renderValue={(value) => {
-        return <TokenValue value={value.join(', ')} />;
-      }}
-      renderExample={(value) => (
-        <span style={{ fontFamily: value.join(', ') }}>{sampleTextShort}</span>
-      )}
-    />
+    <ul className="grid grid-cols-3">
+      {headers.map((header) => (
+        <li>{header}</li>
+      ))}
+    </ul>
   );
 }
