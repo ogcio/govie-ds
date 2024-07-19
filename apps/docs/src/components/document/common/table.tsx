dbeaca645dbd7f0deeb46986a3b814ac192b1c82
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2024-07-19 08:17:08 +0100

    docs: update table style (#120)

--- apps/docs/src/components/document/common/table.tsx
+++ apps/docs/src/components/document/common/table.tsx
@@ -38,7 +38,7 @@ export function Table<TId>({
   // TODO: review shadow ring-1 ring-black ring-opacity-5
   return (
     <div className="overflow-hidden sm:rounded-lg">
-      <table className="w-full border border-gray-100 table-fixed">
+      <table className="w-full border border-gray-50 table-fixed">
         <thead className="bg-gray-50">
           <Tr>
             {headers.map((header) => (
