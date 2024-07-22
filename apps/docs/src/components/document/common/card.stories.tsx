cd18bc2c82e5fe5791b9451ab9f9726ff326b06c
Ian Lovell <ian.lovell@nearform.com>
2024-07-22 20:56:01 +0100

    wip: add card

--- apps/docs/src/components/document/common/card.stories.tsx
+++ apps/docs/src/components/document/common/card.stories.tsx
@@ -1,5 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { DesignTokensIcon } from '../get-started/design-tokens-icon';
 import { Card } from './card';
+
+function Center({ children }: { children: React.ReactNode }) {
+  return <div className="flex items-center justify-center">{children}</div>;
+}
 
 const meta = {
   title: 'Common/Card',
@@ -12,6 +17,37 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'Card content',
+    children: (
+      <Center>
+        <DesignTokensIcon />
+      </Center>
+    ),
+  },
+};
+
+export const WithTitle: Story = {
+  args: {
+    ...Default.args,
+    title: 'Design tokens',
+  },
+};
+
+export const WithLink: Story = {
+  args: {
+    ...Default.args,
+    link: {
+      href: '#',
+      label: 'Design tokens',
+    },
+  },
+};
+
+export const WithTitleAndLink: Story = {
+  args: {
+    ...WithTitle.args,
+    link: {
+      href: '#',
+      label: 'Design tokens',
+    },
   },
 };
