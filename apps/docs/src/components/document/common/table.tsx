ae687adca6ef8add91b277bd76176a8f34658f01
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2024-07-22 14:45:37 +0100

    docs: update typography tables (#127)

--- apps/docs/src/components/document/common/table.tsx
+++ apps/docs/src/components/document/common/table.tsx
@@ -19,7 +19,7 @@ export function Td({
   style?: React.CSSProperties;
 }) {
   return (
-    <td className={cn('p-xl text-gray-700', className)} style={style}>
+    <td className={cn('p-xl', className)} style={style}>
       {children}
     </td>
   );
@@ -37,7 +37,7 @@ export function Table<TId>({
 }) {
   // TODO: review shadow ring-1 ring-black ring-opacity-5
   return (
-    <div className="overflow-hidden sm:rounded-lg">
+    <div className="overflow-x-auto rounded-lg">
       <table className="w-full border border-gray-50 table-fixed">
         <thead className="bg-gray-50">
           <Tr>
