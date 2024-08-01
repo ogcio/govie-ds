92da8899303d98b4f4628656e1c96e67da88ca13
Ian Lovell <ian.lovell@nearform.com>
2024-08-01 15:41:30 +0100

    wip: add files

---
+++ packages/html/ds/src/header/header.ts
@@ -0,0 +1,7 @@
+export function initHeader() {
+  document
+    .querySelectorAll('[data-module="gieds-header"]')
+    .forEach((headerElement) => {
+      console.log({ headerElement });
+    });
+}
---
+++ packages/html/ds/vite.config.ts
@@ -0,0 +1,45 @@
+import { extname, relative } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import react from '@vitejs/plugin-react';
+import { glob } from 'glob';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ['src'], exclude: ['src/**/*.stories.tsx'] }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync('src/**/*.{ts,tsx}', {
+            ignore: ['src/**/*.d.ts', 'src/**/*.stories.tsx'],
+          })
+          .map((file: string) => [
+            // 1. The name of the entry point
+            // src/nested/foo.js becomes nested/foo
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
