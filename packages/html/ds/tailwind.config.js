14ea4e2f48d3c6ba6fe42a09b19beaa68dba11a0
Ian Lovell <ian.lovell@nearform.com>
2024-08-05 13:52:53 +0100

    wip: update

--- packages/html/ds/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@govie-ds/tailwind";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.html"],
-  theme: createTheme(),
-  plugins: [],
-};
