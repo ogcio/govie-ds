6ad18249670cd990091cb9f1b3155bdba6cfd4fb
Ian Lovell <ian.lovell@nearform.com>
2024-08-06 12:02:54 +0100

    wip: update

--- packages/html/ds/tailwind.config.ts
+++ packages/html/ds/tailwind.config.ts
@@ -2,6 +2,7 @@ import { createTheme } from '@govie-ds/tailwind';
 import { Config } from 'tailwindcss';
 
 const config: Config = {
+  prefix: 'gi-',
   content: ['./src/**/*.html'],
   theme: createTheme(),
   plugins: [],
--- packages/html/storybook/tailwind.config.ts
+++ packages/html/storybook/tailwind.config.ts
@@ -2,6 +2,7 @@ import { Config } from "tailwindcss";
 import { createTheme } from "@govie-ds/tailwind";
 
 const config: Config = {
+  prefix: "gi-",
   content: ["../ds/src/**/*.html"],
   theme: createTheme(),
   plugins: [],
