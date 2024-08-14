496048fdaf61c2bc4db87876fef0d0983f3d37c3
Ian Lovell <ian.lovell@nearform.com>
2024-08-14 18:20:48 +0100

    wip: update

--- packages/html/ds/vite.config.ts
+++ packages/html/ds/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { processMacrosPlugin } from './scripts/process-macros-plugin';
-import { processSchemasPlugin } from './scripts/process-schemas-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dts({ include: ['src'], exclude: ['src/**/*.stories.ts'] }),
     processMacrosPlugin(),
-    processSchemasPlugin(),
   ],
   build: {
     copyPublicDir: false,
