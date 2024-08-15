f0e47eff51837ec3e40a24b9972fd5f3fdf2851c
BogdanDanilescu <153850559+BogdanDanilescu@users.noreply.github.com>
2024-08-15 17:21:49 +0300

    docs: add link to contact page (#163)
    Co-authored-by: Ian Lovell <102812633+ianlnf@users.noreply.github.com>

--- apps/docs/src/app/contact/page.tsx
+++ apps/docs/src/app/contact/page.tsx
@@ -1,11 +1,21 @@
 import { Heading, Paragraph } from '@govie-react/ds';
+import { config } from '@/lib/config';
 
 export default function ContactPage() {
   return (
     <div>
       <Heading>Contact</Heading>
       <Paragraph>
-        You can contact the design system team via email. Email coming soon.
+        You can contact the design system team using{' '}
+        {/* TODO: replace the a tag with <Link> when target prop is implemented */}
+        <a
+          target="_blank"
+          className="underline text-blue-700 hover:decoration-md"
+          href={config.signUpFormUrl}
+        >
+          this form
+        </a>
+        .
       </Paragraph>
     </div>
   );
