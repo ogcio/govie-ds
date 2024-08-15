cec34e077905f88e360a63111f8c8af9863ffb42
bogdan <bogdan.danilescu@colibridigital.io>
2024-08-15 15:37:57 +0300

    add link to contact page

--- apps/docs/src/app/contact/page.tsx
+++ apps/docs/src/app/contact/page.tsx
@@ -1,11 +1,19 @@
 import { Heading, Paragraph } from '@govie-react/ds';
+import { config } from '@/lib/config';
 
 export default function ContactPage() {
   return (
     <div>
       <Heading>Contact</Heading>
       <Paragraph>
-        You can contact the design system team via email. Email coming soon.
+        You can contact the design system team using{' '}
+        <a
+          target="_blank"
+          className="underline text-blue-700 hover:decoration-md"
+          href={config.signUpFormUrl}
+        >
+          this form.
+        </a>
       </Paragraph>
     </div>
   );
