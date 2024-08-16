e2a877b4d6c28467f85af10111539d94b6842fdd
Ian Lovell <ian.lovell@nearform.com>
2024-08-16 11:17:17 +0100

    wip: update

--- packages/html/ds/src/header/header.test.ts
+++ packages/html/ds/src/header/header.test.ts
@@ -1,5 +1,32 @@
+import { renderMacro } from '@govie-frontend/macro';
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+} from '@testing-library/dom';
+import html from './header.html?raw';
+import { HeaderProps } from './header.schema';
+
+function renderContent<TProps>({ name, html }: { name: string; html: string }) {
+  return function foo(props: TProps) {
+    const markup = renderMacro<TProps>({ name, html })(props);
+
+    const div = document.createElement('div');
+    div.innerHTML = markup;
+    return div;
+  };
+}
+
 describe('header', () => {
+  const renderHeader = renderContent<HeaderProps>({
+    name: 'govieHeader',
+    html,
+  });
+
   it('should work', () => {
-    expect(true).toBe(true);
+    const foo = renderHeader({ title: 'Application service' });
+
+    expect(getByText(foo, 'Application service')).toBeTruthy();
   });
 });
