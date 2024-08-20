ca131e0ac43a8897608749fe9af1b94f39c160ba
BogdanDanilescu <bogdan.danilescu@colibridigital.io>
2024-08-20 14:03:28 +0300

    add pages for footer content

--- apps/docs/src/components/footer/footer.tsx
+++ apps/docs/src/components/footer/footer.tsx
@@ -2,45 +2,37 @@ import { Container, Link } from '@govie-react/ds';
 import Image from 'next/image';
 import styles from './footer.module.css';
 import footerImage from './logo.png';
-import { OglLogo } from './ogl-logo';
 
 // INFO: This Footer component along with its styles is a temporary solution until we implement the Footer component from @govie-react/ds. The styles will also become redundant after we utilise the external Footer component
 // TODO: Replace this Footer component with the Footer component from @govie-react/ds
-// TODO: Create releveant pages related to the footer section in mdx ( content folder ) after getting more info from Content/Tamara
-// TODO: Replace de MockLinks and MockLinkLong consts with relevant data
+// TODO: Review the content of each page from FooterLinks and update accordingly
 
-const MockLinks = [
+const footerLinks = [
   {
     label: 'Help',
-    href: 'https://ogcio.github.io/ogcio-ds-website/help/',
+    href: '/help/',
   },
   {
     label: 'Privacy Policy',
-    href: 'https://ogcio.github.io/ogcio-ds-website/privacy-policy/',
+    href: '/privacy-policy/',
   },
   {
     label: 'Cookies Policy',
-    href: 'https://ogcio.github.io/ogcio-ds-website/cookies-policy/',
+    href: '/cookies-policy/',
   },
   {
     label: 'Accessibility statement',
-    href: 'https://ogcio.github.io/ogcio-ds-website/accessibility/',
+    href: '/accessibility-statement/',
   },
   {
     label: 'Contact',
-    href: 'https://ogcio.github.io/ogcio-ds-website/contact/',
+    href: '/contact/',
   },
   {
     label: 'Government digital service',
-    href: 'https://ogcio.github.io/ogcio-ds-website/government-digital-service/',
+    href: '/government-digital-service/',
   },
 ];
-
-const MockLinkLong = {
-  label:
-    'All content is available under the Open Government Licence v3.0, except where otherwise stated',
-  href: 'https://ogcio.github.io/ogcio-ds-website/government-digital-service/#',
-};
 
 export function Footer() {
   return (
@@ -49,18 +41,13 @@ export function Footer() {
         <div className={styles.footerInner}>
           <div className={styles.footerLinksContainer}>
             <ul className={styles.footerLinkList}>
-              {MockLinks.map((link, id) => (
+              {footerLinks.map((link, id) => (
                 <li key={id}>
                   <Link href={link.href}>{link.label}</Link>
                 </li>
               ))}
             </ul>
-            <div className={styles.footerLogoContainer}>
-              {OglLogo()}
-              <Link href={MockLinkLong.href}>{MockLinkLong.label}</Link>
-            </div>
           </div>
-
           <Image
             src={footerImage}
             alt="GOV IE"
