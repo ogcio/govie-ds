af1207084043c32cfc4844420b3a1e0b69a5a959
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2024-08-20 12:19:05 +0100

    test: add html vitest (#168)

--- packages/html/ds/scripts/process-macros-plugin.ts
+++ packages/html/ds/scripts/process-macros-plugin.ts
@@ -1,82 +1,12 @@
 import path from 'node:path';
 import fs from 'fs-extra';
 import { glob } from 'glob';
-import * as properties from '../src/dist/properties.js';
+import { addMacroValidation } from '../src/common/validation';
 
 type MacroDestination = {
   engine: 'nunjucks' | 'jinja';
   mode: 'dev' | 'prod';
 };
-
-function injectValidation({
-  macroHtml,
-  validationMarkup,
-}: {
-  macroHtml: string;
-  validationMarkup: string;
-}) {
-  const macroRegex = /({% macro [^%]*?%}\s*)/i;
-  return macroHtml.replace(
-    macroRegex,
-    (match) => `${match}${validationMarkup}`,
-  );
-}
-
-function injectJinjaValidation({
-  macroHtml,
-  requiredKeys,
-}: {
-  macroHtml: string;
-  requiredKeys: string[];
-}) {
-  const validationMarkup = `
-{% set required_keys = [${requiredKeys.map((key) => `'${key}'`).join(',')}] %}
-  {% for key in required_keys %}
-      {% if key not in props %}
-          {% set error_message = "Missing required property '" ~ key ~ "'." %}
-          {{ throw(error_message) }}
-      {% endif %}
-{% endfor %}
-  `;
-
-  return injectValidation({ macroHtml, validationMarkup });
-}
-
-function processContent({
-  engine,
-  mode,
-  content,
-  macroName,
-}: {
-  engine: string;
-  mode: string;
-  content: string;
-  macroName: string;
-}) {
-  if (mode === 'prod') {
-    return content;
-  }
-
-  switch (engine) {
-    case 'nunjucks': {
-      // TODO: inject nunjucks validation
-      return content;
-    }
-    case 'jinja': {
-      const requiredKeys: string[] = (properties[macroName] ?? [])
-        .filter((property) => property.required)
-        .map((property) => property.name);
-
-      return injectJinjaValidation({
-        macroHtml: content,
-        requiredKeys,
-      });
-    }
-    default: {
-      throw new Error(`Unsupported engine '${engine}'.`);
-    }
-  }
-}
 
 export function processMacrosPlugin() {
   return {
@@ -110,12 +40,15 @@ export function processMacrosPlugin() {
             file.replace(path.basename(file), 'macro.html'),
           );
 
-          const updatedContent = processContent({
-            engine: destination.engine,
-            mode: destination.mode,
-            content,
-            macroName: path.basename(file, '.html'),
-          });
+          const updatedContent =
+            destination.mode === 'dev'
+              ? addMacroValidation({
+                  engine: destination.engine,
+                  mode: destination.mode,
+                  content,
+                  macroName: path.basename(file, '.html'),
+                })
+              : content;
 
           await fs.ensureDir(path.dirname(destinationPath));
           await fs.writeFile(destinationPath, updatedContent);
