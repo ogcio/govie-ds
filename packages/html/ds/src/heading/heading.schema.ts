f88190bcc2f1a726843828f13aa1cc08ae31e4b3
BogdanDanilescu <bogdan.danilescu@colibridigital.io>
2024-08-23 13:51:45 +0300

    include zod enums to build properties fct, add descriptions to properties

--- packages/html/ds/src/heading/heading.schema.ts
+++ packages/html/ds/src/heading/heading.schema.ts
@@ -8,19 +8,32 @@ export enum SIZE {
 }
 
 export enum TAG {
-    H1 = 'H1',
-    H2 = 'H2',
-    H3 = 'H3',
-    H4 = 'H4',
-    H5 = 'H5',
-    H6 = 'H6',
-  }
+  H1 = 'H1',
+  H2 = 'H2',
+  H3 = 'H3',
+  H4 = 'H4',
+  H5 = 'H5',
+  H6 = 'H6',
+}
 
 export const headingSchema = zod.object({
-  size: zod.nativeEnum(SIZE),
-  tag: zod.nativeEnum(TAG),
-  text: zod.string(),
-  caption: zod.string().optional(),
+  size: zod.nativeEnum(SIZE, {
+    description: 'Options for the size of the heading',
+    required_error: 'Option is required',
+  }),
+  tag: zod.nativeEnum(TAG, {
+    description: 'Options for the tag element of the heading',
+    required_error: 'A tag element is required',
+  }),
+  text: zod.string({
+    description: 'Ttitle of the heading',
+    required_error: 'Title is required',
+  }),
+  caption: zod
+    .string({
+      description: 'Caption of the heading',
+    })
+    .optional(),
 });
 
 export type HeadingProps = zod.infer<typeof headingSchema>;
