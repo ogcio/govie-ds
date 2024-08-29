0a5a3d1d4db364b5a8040aa3dd6ec968fe46599d
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-08-29 10:23:54 +0200

    chore: update link component to tailwind

--- packages/react/ds/src/link/link.tsx
+++ packages/react/ds/src/link/link.tsx
@@ -1,14 +1,40 @@
-import styles from './link.module.css';
-
 export type LinkProps = {
   as?: React.ElementType;
   href: string;
   children: React.ReactNode;
+  noVisited?: boolean;
+  noUnderline?: boolean;
+  external?: boolean;
+  dark?: boolean;
 };
 
-export function Link({ as: Component = 'a', href, children }: LinkProps) {
+export function Link({
+  as: Component = 'a',
+  href,
+  children,
+  noUnderline,
+  noVisited,
+  external,
+}: LinkProps) {
   return (
-    <Component href={href} className={styles.link}>
+    <Component
+      href={href}
+      {...(external ? { rel: 'noreferrer noopener' } : {})}
+      {...(external ? { target: '_blank' } : {})}
+      className={`
+        ${noUnderline ? '' : 'gi-underline gi-underline-offset-[0.1em]'}
+        ${noVisited ? 'visited:gi-text-blue-700' : 'visited:gi-text-purple-700'}
+        gi-text-blue-700
+        gi-decoration-[max(1px,0.0625rem)]
+        hover:gi-text-blue-800
+        hover:gi-decoration-skip-ink-none
+        hover:gi-decoration-[max(3px,0.1875rem,0.12em)]
+        focus:gi-outline
+        focus:gi-outline-transparent
+        focus:gi-bg-yellow-400
+        focus:gi-outline-[3px]
+        focus:gi-shadow-[0_-2px_var(--gieds-color-yellow-400),0_4px_var(--gieds-color-gray-900)]`}
+    >
       {children}
     </Component>
   );
