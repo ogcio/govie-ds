94c19ec0ee687919f6bed8438e857b9050a44298
BogdanDanilescu <bogdan.danilescu@colibridigital.io>
2024-08-30 12:57:58 +0300

    resolve macros import

--- packages/html/ds/src/footer/footer.stories.ts
+++ packages/html/ds/src/footer/footer.stories.ts
@@ -2,7 +2,10 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { renderComponent } from '../storybook/storybook';
 import html from './footer.html?raw';
 
-const macro = { name: 'govieFooter', html };
+// Name of the folder the macro resides
+const path = import.meta.url.split('/footer')[0];
+
+const macro = { name: 'govieFooter', html, path };
 
 const Footer = renderComponent(macro);
 
@@ -18,7 +21,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-
-
-
-
