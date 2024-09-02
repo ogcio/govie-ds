f55baab2251a493b596406f426c331813808bb6b
Hamza Khan <164040832+hamza14khan@users.noreply.github.com>
2024-09-02 10:03:39 +0100

    feat: add html link component (#184)

---
+++ packages/html/ds/src/link/link.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { renderComponent } from '../storybook/storybook';
+import html from './link.html?raw';
+import { LinkProps } from './link.schema';
+
+const macro = { name: 'govieLink', html };
+
+const Link = renderComponent<LinkProps>(macro);
+
+const meta = {
+  component: Link,
+  title: 'Navigation/Link',
+  parameters: {
+    macro,
+  },
+} satisfies Meta<typeof Link>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  argTypes: {
+    label: {
+      control: 'text',
+      type: { name: 'string', required: true },
+    },
+    href: {
+      control: 'text',
+      type: { name: 'string', required: true },
+    },
+    noUnderline: {
+      description: 'To remove underlines from links.',
+      control: 'boolean',
+      type: { name: 'boolean' },
+    },
+    external: {
+      description: 'To open the link in a new tab.',
+      control: 'boolean',
+      type: { name: 'boolean' },
+    },
+    noVisited: {
+      description:
+        'Where it is not helpful to distinguish between visited and unvisited states, for example when linking to pages with frequently-changing content such as the dashboard for an admin interface.',
+      control: 'boolean',
+      type: { name: 'boolean' },
+    },
+    dark: {
+      description:
+        'Show white links on dark backgrounds — for example, in headers, custom components, and patterns with darker backgrounds.',
+      control: 'boolean',
+      type: { name: 'boolean' },
+    },
+  },
+  args: {
+    href: '#',
+    label: 'Link',
+  },
+};
+
+export const WithoutUnderline: Story = {
+  args: {
+    href: '#',
+    label: 'Link without underline',
+    noUnderline: true,
+  },
+};
+
+export const External: Story = {
+  args: {
+    href: '#',
+    label: 'Link text (opens in a new tab)',
+    external: true,
+  },
+};
+
+export const NoVisited: Story = {
+  args: {
+    href: '#',
+    label: 'Link',
+    noVisited: true,
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    href: '#',
+    label: 'Link',
+    dark: true,
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: '#1d70b8' }],
+    },
+  },
+};
