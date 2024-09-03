6cf5b6d02ecbeec540d3969e09b13fd95349a75a
BogdanDanilescu <153850559+BogdanDanilescu@users.noreply.github.com>
2024-09-03 12:37:54 +0300

    Implement heading component (#190)

---
+++ packages/html/ds/src/heading/heading.schema.ts
@@ -0,0 +1,41 @@
+import * as zod from 'zod';
+
+export enum Size {
+  Smallest = '2xs',
+  ExtraSmall = 'xs',
+  Small = 'sm',
+  Medium = 'md',
+  Large = 'lg',
+  ExtraLarge = 'xl',
+}
+
+export enum Tag {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  H5 = 'h5',
+  H6 = 'h6',
+}
+
+export const headingSchema = zod.object({
+  size: zod.nativeEnum(Size, {
+    description: 'Options for the size of the heading',
+    required_error: 'Option is required',
+  }),
+  tag: zod.nativeEnum(Tag, {
+    description: 'Options for the tag element of the heading',
+    required_error: 'A tag element is required',
+  }),
+  text: zod.string({
+    description: 'Ttitle of the heading',
+    required_error: 'Title is required',
+  }),
+  caption: zod
+    .string({
+      description: 'Caption of the heading',
+    })
+    .optional(),
+});
+
+export type HeadingProps = zod.infer<typeof headingSchema>;
---
+++ packages/html/ds/src/heading/heading.stories.ts
@@ -0,0 +1,130 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { renderComponent } from '../storybook/storybook';
+import html from './heading.html?raw';
+import { Size, Tag } from './heading.schema';
+import { HeadingProps } from './heading.schema';
+
+const macro = { name: 'govieHeading', html };
+
+const Heading = renderComponent<HeadingProps>(macro);
+
+const meta = {
+  component: Heading,
+  title: 'typography/Heading',
+  parameters: {
+    macro,
+  },
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: Size.Medium,
+    tag: Tag.H1,
+    text: 'Heading',
+    caption: '',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: Size.Small,
+    tag: Tag.H1,
+    text: 'Small heading',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: Size.Medium,
+    tag: Tag.H1,
+    text: 'Medium heading',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: Size.Large,
+    tag: Tag.H1,
+    text: 'Large heading',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const ExtraLarge: Story = {
+  args: {
+    size: Size.ExtraLarge,
+    tag: Tag.H1,
+    text: 'Extra large heading',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export const Caption: Story = {
+  args: {
+    size: Size.Medium,
+    tag: Tag.H1,
+    text: 'Heading with h6',
+    caption: 'Caption Text',
+  },
+  argTypes: {
+    size: {
+      options: Object.values(Size),
+      control: { type: 'radio' },
+    },
+    tag: {
+      options: Object.values(Tag),
+      control: { type: 'radio' },
+    },
+  },
+};
