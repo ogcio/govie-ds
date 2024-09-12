daac5c09e0b0562f1c49d121ef21ae632ad8c16c
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-09-12 16:52:16 +0200

    fix: icon-button story

--- packages/react/ds/src/icon-button/icon-button.stories.tsx
+++ packages/react/ds/src/icon-button/icon-button.stories.tsx
@@ -16,21 +16,21 @@ type Story = StoryObj<typeof meta>;
 
 export const Small: Story = {
   args: {
-    icon: <Icon id="thumbs-up" size="sm" />,
+    icon: <Icon icon="thumb_up" size="sm" />,
     ariaLabel: 'Thumbs up',
   },
 };
 
 export const Default: Story = {
   args: {
-    icon: <Icon id="thumbs-up" />,
+    icon: <Icon icon="thumb_up" />,
     ariaLabel: 'Thumbs up',
   },
 };
 
 export const Large: Story = {
   args: {
-    icon: <Icon id="thumbs-up" size="lg" />,
+    icon: <Icon icon="thumb_up" size="lg" />,
     ariaLabel: 'Thumbs up',
   },
 };
@@ -44,14 +44,14 @@ export const ExtraLarge: Story = {
 
 export const Solid: Story = {
   args: {
-    icon: <Icon id="thumbs-up" solid />,
+    icon: <Icon icon="thumb_up" solid />,
     ariaLabel: 'Thumbs up',
   },
 };
 
 export const Disabled: Story = {
   args: {
-    icon: <Icon id="thumbs-up" />,
+    icon: <Icon icon="thumb_up" />,
     ariaLabel: 'Thumbs up',
     disabled: true,
   },
