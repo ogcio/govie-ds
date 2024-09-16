252a24c96bffe090a2aaeb92c8d0fcbe15846e1b
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-09-16 14:27:56 +0200

    chore: fix compile errors

--- packages/html/ds/src/container/container.schema.ts
+++ packages/html/ds/src/container/container.schema.ts
@@ -1,7 +1,11 @@
 import { z as zod } from 'zod';
 
 export const containerSchema = zod.object({
-  html: zod.string().optional(),
+  html: zod
+    .string({
+      description: 'HTML content of the container',
+    })
+    .optional(),
 });
 
 export type ContainerProps = zod.infer<typeof containerSchema>;
