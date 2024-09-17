f492bdf42ce624d4dba314dda8278324a7f03762
hamza14khan <hamzaubit14@gmail.com>
2024-09-17 18:48:54 +0100

    feat: add text input

---
+++ packages/html/ds/src/text-input/text-input.schema.ts
@@ -0,0 +1,42 @@
+import * as zod from 'zod';
+
+export const textInputSchema = zod.object({
+  hasError: zod
+    .boolean({
+      description:
+        'Indicates whether the input should be displayed in an error state. When `true`, the border color will be red.',
+    })
+    .optional(),
+  prefix: zod
+    .string({
+      description:
+        'Element or text to display on the left side of the input, such as a unit or symbol.',
+    })
+    .optional(),
+  suffix: zod
+    .string({
+      description:
+        'Element or text to display on the right side of the input, such as a unit or symbol.',
+    })
+    .optional(),
+  halfFluid: zod
+    .boolean({
+      description:
+        'When `true`, the input width is set to 50% of the available space.',
+    })
+    .optional(),
+  fullFluid: zod
+    .boolean({
+      description:
+        'When `true`, the input width is set to 100% of the available space.',
+    })
+    .optional(),
+  characterWidth: zod
+    .number({
+      description:
+        'Sets the width of the input in terms of the number of characters it can contain.',
+    })
+    .optional(),
+});
+
+export type TextInputProps = zod.infer<typeof textInputSchema>;
---
+++ packages/html/ds/src/text-input/text-input.stories.ts
@@ -0,0 +1,91 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { renderComponent } from '../storybook/storybook';
+import html from './text-input.html?raw';
+import { TextInputProps } from './text-input.schema';
+
+const macro = { name: 'govieTextInput', html };
+
+const TextInput = renderComponent<TextInputProps>(macro);
+
+const meta = {
+  title: 'Form/TextInput',
+  parameters: {
+    macro,
+    docs: {
+      description: {
+        component:
+          'Use the text input component when you need to let users enter text that’s no longer than a single line, such as their name or phone number. Use the `halfFluid`, `fullFluid`, or `characterWidth` properties to control the width of the input field based on different design needs.',
+      },
+    },
+  },
+  component: TextInput,
+  argTypes: {
+    hasError: {
+      description:
+        'Indicates whether the input should be displayed in an error state. When `true`, the border color will be red.',
+      control: 'boolean',
+      table: {
+        category: 'Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    prefix: {
+      description:
+        'Element or text to display on the left side of the input, such as a unit or symbol.',
+      control: 'text',
+      table: {
+        category: 'Content',
+        type: { summary: 'string' },
+        defaultValue: { summary: '-' },
+      },
+    },
+    suffix: {
+      description:
+        'Element or text to display on the right side of the input, such as a unit or symbol.',
+      control: 'text',
+      table: {
+        category: 'Content',
+        type: { summary: 'string' },
+        defaultValue: { summary: '-' },
+      },
+    },
+    halfFluid: {
+      description:
+        'When `true`, the input width is set to 50% of the available space.',
+      control: 'boolean',
+      table: {
+        category: 'Width Control',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    fullFluid: {
+      description:
+        'When `true`, the input width is set to 100% of the available space.',
+      control: 'boolean',
+      table: {
+        category: 'Width Control',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+    characterWidth: {
+      description:
+        'Sets the width of the input in terms of the number of characters it can contain.',
+      control: 'number',
+      table: {
+        category: 'Width Control',
+        type: { summary: 'number' },
+        defaultValue: { summary: '-' },
+      },
+    },
+  },
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
