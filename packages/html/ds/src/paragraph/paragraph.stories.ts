e835d5320fe8e4a2c91381437294bb2b5e803f39
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-09-19 15:00:20 +0200

    feat: paragraph improvements (#223)

--- packages/html/ds/src/paragraph/paragraph.stories.ts
+++ packages/html/ds/src/paragraph/paragraph.stories.ts
@@ -1,7 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { renderComponent } from '../storybook/storybook';
 import html from './paragraph.html?raw';
-import { AsEnum, ParagraphProps, SizeEnum } from './paragraph.schema';
+import {
+  AlignEnum,
+  AsEnum,
+  ParagraphProps,
+  SizeEnum,
+  WhitespaceEnum,
+} from './paragraph.schema';
 
 const macro = { name: 'govieParagraph', html };
 
@@ -37,10 +43,69 @@ export const Default: Story = {
       type: { name: 'string', required: false },
       description: 'Specifies the size of the paragraph.',
     },
+    align: {
+      control: 'radio',
+      options: Object.values(AlignEnum),
+      type: { name: 'string', required: false },
+      description: 'Specifies the alignment of the paragraph.',
+    },
+    whitespace: {
+      control: 'radio',
+      options: Object.values(WhitespaceEnum),
+      type: { name: 'string', required: false },
+      description: 'Specifies the whitespace property.',
+    },
   },
   args: {
     content: 'This is a paragraph.',
     as: AsEnum.Paragraph,
     size: SizeEnum.Medium,
+    align: AlignEnum.Left,
+    whitespace: WhitespaceEnum.Normal,
+  },
+};
+
+export const RightAlignment: Story = {
+  args: {
+    content: 'This is a paragraph.',
+    as: AsEnum.Paragraph,
+    size: SizeEnum.Medium,
+    align: AlignEnum.Right,
+  },
+};
+
+export const WhitespacePre: Story = {
+  args: {
+    content: `Hey everyone!
+
+It's almost 2022       and we still don't know if there       is aliens living among us, or do we? Maybe the person writing this is an alien.
+
+You will never know.`,
+    as: AsEnum.Paragraph,
+    whitespace: WhitespaceEnum.Pre,
+  },
+};
+
+export const WhitespacePreWrap: Story = {
+  args: {
+    content: `Hey everyone!
+
+It's almost 2022       and we still don't know if there       is aliens living among us, or do we? Maybe the person writing this is an alien.
+
+You will never know.`,
+    as: AsEnum.Paragraph,
+    whitespace: WhitespaceEnum.PreWrap,
+  },
+};
+
+export const WhitespaceBreakSpaces: Story = {
+  args: {
+    content: `Hey everyone!
+
+It's almost 2022       and we still don't know if there       is aliens living among us, or do we? Maybe the person writing this is an alien.
+
+You will never know.`,
+    as: AsEnum.Paragraph,
+    whitespace: WhitespaceEnum.BreakSpaces,
   },
 };
--- packages/react/ds/src/paragraph/paragraph.tsx
+++ packages/react/ds/src/paragraph/paragraph.tsx
@@ -2,18 +2,66 @@ import { Text } from '../text/text.js';
 
 export type ParagraphAs = 'p' | 'span';
 export type ParagraphSize = 'lg' | 'md' | 'sm';
+export type ParagraphAlign = 'left' | 'center' | 'right' | 'justify';
+export type ParagraphWhitespace =
+  | 'normal'
+  | 'pre'
+  | 'pre-wrap'
+  | 'break-spaces';
 
 export function Paragraph({
   as: As = 'p',
   size = 'md',
+  align = 'left',
+  whitespace = 'normal',
   children,
 }: {
   as?: ParagraphAs;
   size?: ParagraphSize;
+  align?: ParagraphAlign;
+  whitespace?: ParagraphWhitespace;
   children: React.ReactNode;
 }) {
+  const alignClass = (() => {
+    switch (align) {
+      case 'center': {
+        return 'gi-text-center';
+      }
+      case 'right': {
+        return 'gi-text-right';
+      }
+      case 'justify': {
+        return 'gi-text-justify';
+      }
+      default: {
+        return 'gi-text-left';
+      }
+    }
+  })();
+
+  const whitespaceClass = (() => {
+    switch (whitespace) {
+      case 'pre': {
+        return 'gi-whitespace-pre';
+      }
+      case 'pre-wrap': {
+        return 'gi-whitespace-pre-wrap';
+      }
+      case 'break-spaces': {
+        return 'gi-whitespace-break-spaces';
+      }
+      default: {
+        return 'gi-whitespace-normal';
+      }
+    }
+  })();
+
   return (
-    <Text as={As} size={size} className={`gi-text-gray-950 gi-font-primary`}>
+    <Text
+      as={As}
+      size={size}
+      className={`gi-text-gray-950 gi-font-primary ${alignClass} ${whitespaceClass}`}
+    >
       {children}
     </Text>
   );
