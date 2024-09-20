ad9070777373d90448fcff272ba3882187185d6b
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-09-20 15:45:29 +0200

    feat: add tabs for react

---
+++ packages/react/ds/src/tabs/tab-item.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { slugify } from '../utils.js';
+import { Link } from '../link/link.js';
+
+export function TabItem({
+  value,
+  href,
+  checked = false,
+  children,
+}: {
+  value: string;
+  href: string;
+  checked?: boolean;
+  ariaLabel?: string;
+  ariaLabelledby?: string;
+  children: React.ReactNode;
+}) {
+  const valueSlug = slugify(value);
+  return (
+    <>
+      <input
+        name="tabs"
+        type="radio"
+        id={`tab-${valueSlug}`}
+        aria-labelledby={`tab-label-${valueSlug}`}
+        aria-roledescription="tab"
+        defaultChecked={checked}
+        className={`gi-absolute gi-opacity-0`}
+      />
+      <label
+        role="tab"
+        htmlFor={`tab-${valueSlug}`}
+        id={`tab-label-${valueSlug}`}
+        aria-hidden="true"
+        className={`
+          gi-inline-block 
+          gi-cursor-pointer
+          gi-px-5
+          gi-py-2
+          gi-border-solid 
+          gi-border-border-gray-200
+          gi-border-b-xs 
+          gi-relative
+          gi--mb-[1px]
+          gi-mr-1
+          gi-bg-gray-50
+          gi-text-center 
+          gi-text-md
+          gi-underline
+          gi-text-slate-300`}
+      >
+        <Link href={href} noVisited={true} noColor={true}>
+          {children}
+        </Link>
+      </label>
+    </>
+  );
+}
---
+++ packages/react/ds/src/tabs/tabs.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TabItem } from './tab-item.js';
+import { TabPanel } from './tab-panel.js';
+import { Tabs } from './tabs.js';
+
+const meta = {
+  title: 'Navigation/Tabs',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Tabs element navigation',
+      },
+    },
+  },
+  component: Tabs,
+} satisfies Meta<typeof Tabs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: '',
+  },
+  render: (arguments_) => {
+    return (
+      <Tabs {...arguments_}>
+        <TabItem value="tab1" checked>
+          Tab 1
+        </TabItem>
+        <TabItem value="tab2">Tab 2</TabItem>
+        <TabItem value="tab3">Tab 3</TabItem>
+        <TabPanel value="tab1">Tab 1 Content</TabPanel>
+        <TabPanel value="tab2">Tab 2 Content</TabPanel>
+        <TabPanel value="tab3">Tab 3 Content</TabPanel>
+      </Tabs>
+    );
+  },
+};
