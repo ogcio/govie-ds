b49995eddce63bf1e7b23cc0d2f08d4eb6416954
BogdanDanilescu <bogdan.danilescu@colibridigital.io>
2024-09-24 17:07:31 +0300

    add stories, tests

--- packages/html/ds/src/select/select.schema.ts
+++ packages/html/ds/src/select/select.schema.ts
@@ -8,33 +8,39 @@ export const selectSchema = zod.object({
     description: 'A unique ID for the select component',
     required_error: 'A value is required',
   }),
-  label: labelSchema,
-  options: zod.object({
-    default: zod.object({
-      label: zod.string({
-        description: 'Label for default option',
-        required_error: 'The label for default error is required',
-      }),
-      value: zod.string({
-        description: 'Value for the default option',
-      }),
-    }),
-    items: zod
-      .object({
-        label: zod.string({
-          description: 'Label for the option',
-          required_error: 'The label for the option is required',
-        }),
-        value: zod.string({
-          description: 'Value for the option',
-          required_error: 'The value for the option is required',
-        }),
-      })
-      .array()
-      .describe('Array of options with labels and values'),
-  }),
-  hint: hintTextSchema.optional(),
-  error: errorTextSchema.optional(),
+  label: labelSchema.describe('Properties for the label component').optional(),
+  options: zod
+    .object({
+      default: zod
+        .object({
+          label: zod.string({
+            description: 'Label for default option',
+            required_error: 'The label for default error is required',
+          }),
+          value: zod.string({
+            description: 'Value for the default option',
+          }),
+        })
+        .describe('The label and value for the default option'),
+      items: zod
+        .object({
+          label: zod.string({
+            description: 'Label for the option',
+            required_error: 'The label for the option is required',
+          }),
+          value: zod.string({
+            description: 'Value for the option',
+            required_error: 'The value for the option is required',
+          }),
+        })
+        .array()
+        .describe('Array of options with labels and values'),
+    })
+    .describe('A list of all the options'),
+  hint: hintTextSchema.describe('Properties for the hint component').optional(),
+  error: errorTextSchema
+    .describe('Properties for the erro component')
+    .optional(),
 });
 
 export type SelectProps = zod.infer<typeof selectSchema>;
--- packages/html/ds/src/select/select.stories.ts
+++ packages/html/ds/src/select/select.stories.ts
@@ -22,13 +22,53 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  argTypes: {
+    id: {
+      description: 'A unqiue valued used to identify the component',
+      control: 'text',
+      table: {
+        category: 'ID',
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+      },
+    },
+    options: {
+      description: 'A list of labels and values for each option',
+      control: 'object',
+      table: {
+        category: 'Options',
+        type: { summary: 'Options' },
+      },
+    },
+    error: {
+      description: 'Error message for the select',
+      control: 'object',
+      table: {
+        category: 'Error',
+        type: { summary: 'ErrorText' },
+      },
+    },
+    hint: {
+      description: 'Hint message for the select',
+      control: 'object',
+      table: {
+        category: 'Hint',
+        type: { summary: 'HintText' },
+      },
+    },
+    label: {
+      description: 'Label associated for the select',
+      control: 'object',
+      table: {
+        category: 'Label',
+        type: { summary: 'Label' },
+      },
+    },
+  },
   args: {
     id: 'Unique-ID',
     label: {
-      content: 'Main Heading',
-    },
-    error: {
-      content: 'This is the error',
+      content: 'Label',
     },
     options: {
       default: {
@@ -47,6 +87,70 @@ export const Default: Story = {
         {
           label: 'Option 3',
           value: 'value-3',
+        },
+      ],
+    },
+  },
+};
+
+export const withHint: Story = {
+  args: {
+    id: 'Unique-ID',
+    label: {
+      content: 'Default Select',
+    },
+    hint: {
+      content: 'This can be different to where you went before',
+    },
+    options: {
+      default: {
+        label: 'Default select',
+        value: 'value-1',
+      },
+      items: [
+        {
+          label: 'Option 1',
+          value: 'value-2',
+        },
+        {
+          label: 'Option 2',
+          value: 'value-3',
+        },
+        {
+          label: 'Option 3',
+          value: 'value-4',
+        },
+      ],
+    },
+  },
+};
+
+export const withError: Story = {
+  args: {
+    id: 'Unique-ID',
+    label: {
+      content: 'Default Select',
+    },
+    error: {
+      content: 'Error message',
+    },
+    options: {
+      default: {
+        label: 'Default select',
+        value: 'value-1',
+      },
+      items: [
+        {
+          label: 'Option 1',
+          value: 'value-2',
+        },
+        {
+          label: 'Option 2',
+          value: 'value-3',
+        },
+        {
+          label: 'Option 3',
+          value: 'value-4',
         },
       ],
     },
