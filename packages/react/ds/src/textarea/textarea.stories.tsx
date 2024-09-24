9bf4c6fc7efa8f924b46cc4ea64082c0ea53c18a
hamza14khan <hamzaubit14@gmail.com>
2024-09-24 16:01:04 +0100

    chore: update textarea component with hint, error and label props

--- packages/react/ds/src/textarea/textarea.stories.tsx
+++ packages/react/ds/src/textarea/textarea.stories.tsx
@@ -1,8 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { ErrorText } from '../error-text/error-text.js';
-import { FormGroup } from '../form-group/form-group.js';
-import { HintText } from '../hint-text/hint-text.js';
-import { Label } from '../label/label.js';
 import { TextArea } from './textarea.js';
 
 const meta = {
@@ -11,20 +7,36 @@ const meta = {
     docs: {
       description: {
         component:
-          'Use the TextArea component when users need to enter longer or multiline text, such as comments or feedback. The `rows` and `cols` properties can be used to control the size of the textarea.',
+          'Use the TextArea component when users need to enter longer or multiline text, such as comments or feedback. The `rows` and `cols` properties control the size of the textarea.',
       },
     },
   },
   component: TextArea,
   argTypes: {
-    hasError: {
-      description:
-        'Indicates whether the textarea should be displayed in an error state. When `true`, the border color will be red.',
-      control: 'boolean',
+    label: {
+      description: 'Label associated with the textarea',
+      control: 'object',
       table: {
-        category: 'Appearance',
-        type: { summary: 'boolean' },
-        defaultValue: { summary: 'false' },
+        category: 'Label',
+        type: { summary: 'Label' },
+      },
+    },
+    hintText: {
+      description:
+        'Hint text for the textarea to provide additional information.',
+      control: 'object',
+      table: {
+        category: 'Hint',
+        type: { summary: 'HintText' },
+      },
+    },
+    errorText: {
+      description:
+        'Error message for the textarea, displayed when there is a validation error.',
+      control: 'object',
+      table: {
+        category: 'Error',
+        type: { summary: 'ErrorText' },
       },
     },
     rows: {
@@ -33,7 +45,7 @@ const meta = {
       table: {
         category: 'Size',
         type: { summary: 'number' },
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: '4' },
       },
     },
     cols: {
@@ -42,7 +54,7 @@ const meta = {
       table: {
         category: 'Size',
         type: { summary: 'number' },
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: '100' },
       },
     },
     autoComplete: {
@@ -60,7 +72,6 @@ const meta = {
       table: {
         category: 'Ref',
         type: { summary: 'React.Ref<HTMLTextAreaElement>' },
-        defaultValue: { summary: '-' },
       },
     },
   },
@@ -70,55 +81,75 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: (_arguments) => (
-    <FormGroup>
-      <Label htmlFor="textarea">Textarea</Label>
-      <TextArea id="textarea" {..._arguments} />
-    </FormGroup>
-  ),
-  args: {},
+  args: {
+    rows: 4,
+    cols: 100,
+    id: 'textarea-id',
+    label: {
+      content: 'Textarea Label',
+      htmlFor: 'textarea-id',
+    },
+    errorText: {
+      content: '',
+    },
+    hintText: {
+      content: '',
+    },
+  },
 };
 
 export const WithLabelAndHint: Story = {
-  render: (_arguments) => (
-    <FormGroup>
-      <Label htmlFor="textarea">Label</Label>
-      <HintText>Hint</HintText>
-      <TextArea id="textarea" {..._arguments} />
-    </FormGroup>
-  ),
-  args: {},
+  args: {
+    id: 'textarea-id',
+    label: {
+      content: 'Label',
+      htmlFor: 'textarea-id',
+    },
+    hintText: {
+      content: 'Hint: This is a helpful hint.',
+    },
+    rows: 4,
+    cols: 100,
+  },
 };
 
 export const WithLabelAndError: Story = {
-  render: (_arguments) => (
-    <FormGroup hasError={true}>
-      <Label htmlFor="textarea">Label</Label>
-      <ErrorText>Error</ErrorText>
-      <TextArea hasError id="textarea" {..._arguments} />
-    </FormGroup>
-  ),
-  args: {},
+  args: {
+    id: 'textarea-id',
+    label: {
+      content: 'Label',
+      htmlFor: 'textarea-id',
+    },
+    errorText: {
+      content: 'Error: Please correct this issue.',
+    },
+  },
 };
 
 export const WithLabelHintAndError: Story = {
-  render: (_arguments) => (
-    <FormGroup hasError>
-      <Label htmlFor="textarea">Label</Label>
-      <HintText>Hint</HintText>
-      <ErrorText>Error</ErrorText>
-      <TextArea hasError id="textarea" {..._arguments} />
-    </FormGroup>
-  ),
-  args: {},
+  args: {
+    id: 'textarea-id',
+    label: {
+      content: 'Label',
+      htmlFor: 'textarea-id',
+    },
+    hintText: {
+      content: 'Hint: This is a helpful hint.',
+    },
+    errorText: {
+      content: 'Error: Please correct this issue.',
+    },
+  },
 };
 
 export const CustomRowsAndColumns: Story = {
-  render: (_arguments) => (
-    <FormGroup>
-      <Label htmlFor="textarea">Custom Rows (6) and Columns (40)</Label>
-      <TextArea id="textarea" rows={6} cols={40} {..._arguments} />
-    </FormGroup>
-  ),
-  args: {},
+  args: {
+    id: 'textarea-id',
+    label: {
+      content: 'Label',
+      htmlFor: 'textarea-id',
+    },
+    rows: 6,
+    cols: 40,
+  },
 };
--- packages/react/ds/src/textarea/textarea.tsx
+++ packages/react/ds/src/textarea/textarea.tsx
@@ -1,38 +1,65 @@
 import React from 'react';
+import { ErrorText, ErrorTextProps } from '../error-text/error-text.js';
+import { HintText, HintTextProps } from '../hint-text/hint-text.js';
+import { Label, LabelProps } from '../label/label.js';
 
 // Extend `React.TextareaHTMLAttributes<HTMLTextAreaElement>` so that
 // the component can accept all the standard attributes and events that a `<textarea>` element can handle.
 export type TextAreaProps =
   React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
     ref?: React.Ref<HTMLTextAreaElement>;
-    hasError?: boolean;
     rows?: number;
     cols?: number;
     autoComplete?: string;
+    errorText?: ErrorTextProps;
+    hintText?: HintTextProps;
+    label?: LabelProps;
   };
 
 // Use React.forwardRef to support refs properly
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   (
     {
-      hasError,
       rows = 4, // default row count
       cols = 100, // default column count
       autoComplete = 'on', // default autoComplete behavior
+      label,
+      errorText,
+      hintText,
+      id,
       ...props
     },
     ref,
   ) => {
+    const hasError = errorText && errorText.content;
     return (
-      <div className="gi-flex gi-items-center">
-        <textarea
-          rows={rows}
-          cols={cols}
-          autoComplete={autoComplete}
-          className={`${hasError ? 'gi-border-red-600' : 'gi-border-gray-950'} gi-flex-initial gi-border-sm gi-border-solid gi-box-border gi-p-1 focus:gi-outline focus:gi-outline-[3px] focus:gi-border-lg focus:gi-border-gray-950 focus:gi-outline-yellow-400 focus:gi-outline-offset-0 xs:gi-text-md gi-text-sm gi-resize-y gi-min-h-10`}
-          ref={ref}
-          {...props}
-        />
+      <div
+        className={`gi-pt-2 gi-mb-4 ${hasError ? 'gi-px-4 gi-border-solid gi-border-l-lg gi-border-red-600' : ''}`}
+      >
+        {label && (
+          <Label size={label.size} htmlFor={id}>
+            {label.content}
+          </Label>
+        )}
+
+        {hintText && (
+          <HintText size={hintText.size}>{hintText.content}</HintText>
+        )}
+
+        {hasError && errorText && (
+          <ErrorText size={errorText.size}>{errorText.content}</ErrorText>
+        )}
+        <div className="gi-flex gi-items-center">
+          <textarea
+            id={id} // Bind the id for label association
+            rows={rows}
+            cols={cols}
+            autoComplete={autoComplete}
+            className={`${hasError ? 'gi-border-red-600' : 'gi-border-gray-950'} gi-flex-initial gi-border-sm gi-border-solid gi-box-border gi-p-1 focus:gi-outline focus:gi-outline-[3px] focus:gi-border-lg focus:gi-border-gray-950 focus:gi-outline-yellow-400 focus:gi-outline-offset-0 xs:gi-text-md gi-text-sm gi-resize-y gi-min-h-10`}
+            ref={ref}
+            {...props}
+          />
+        </div>
       </div>
     );
   },
