8f676eeeb269e90ec1ddb1dff4a3c1915f234496
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-09-24 16:51:27 +0200

    feat: add tabs for react (#228)

---
+++ packages/react/ds/src/tabs/tabs.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function Tabs({
+  ariaLabel,
+  ariaLabelledby,
+  children,
+}: {
+  ariaLabel?: string;
+  ariaLabelledby?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      role="tablist"
+      aria-label={ariaLabel}
+      aria-labelledby={ariaLabelledby}
+      className="gi-flex gi-flex-wrap"
+    >
+      {children}
+    </div>
+  );
+}
---
+++ packages/react/ds/src/utils.ts
@@ -0,0 +1,20 @@
+/**
+ * Returns a safe for url string representation.
+ *
+ * @param value - The string to be transformed
+ * @returns A string that can be used as slug
+ *
+ */
+export function slugify(value?: string) {
+  if (!value) {
+    return value;
+  }
+
+  let slug = value.replaceAll(/^\s+|\s+$/g, '');
+  slug = slug.toLowerCase();
+  slug = slug
+    .replaceAll(/[^\d a-z-]/g, '')
+    .replaceAll(/\s+/g, '-')
+    .replaceAll(/-+/g, '-');
+  return slug;
+}
