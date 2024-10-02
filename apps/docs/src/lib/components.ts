a8b40d2bf80a44713dc09880dbd31eda82f31a86
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-10-02 11:41:06 +0200

    chore: update status

--- apps/docs/src/lib/components.ts
+++ apps/docs/src/lib/components.ts
@@ -700,14 +700,14 @@ export function getComponents(): ComponentDetail[] {
             id: 'global',
             href: '?path=/docs/typography-paragraph--docs',
           },
-          status: 'alpha',
+          status: 'beta',
         },
         {
           platform: {
             id: 'react',
             href: '?path=/docs/typography-paragraph--docs',
           },
-          status: 'alpha',
+          status: 'beta',
         },
       ],
     },
