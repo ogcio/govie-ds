9f9ce9f20a29ca2575f5060a2ed886f77a1a34e6
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-10-09 16:09:37 +0300

    add relevant props, style radios

--- packages/html/ds/src/radio/radio.schema.ts
+++ packages/html/ds/src/radio/radio.schema.ts
@@ -12,6 +12,9 @@ export const radiosSchema = zod.object({
     description: 'An unique ID given to the group of radios',
     required_error: 'The unique ID is required',
   }),
+  inline: zod.boolean({
+    description: "specify if the radios are inline"
+  }).optional(),
   items: zod
     .object({
       label: zod
--- packages/html/ds/src/radio/radio.stories.ts
+++ packages/html/ds/src/radio/radio.stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Size, Tag } from '../heading/heading.schema';
 import { renderComponent } from '../storybook/storybook';
+import { RadiosProps, RadioSizeEnum } from './radio.schema';
 import html from './radios-group.html?raw';
-// import { CheckboxesProps } from './checkboxes.schema';
 
 // Name of the folder the macro resides
 const path = import.meta.url.split('/radio')[0];
 
 const macro = { name: 'govieRadiosGroup', html, path };
 
-const Radios = renderComponent(macro);
+const Radios = renderComponent<RadiosProps>(macro);
 
 const meta = {
   component: Radios,
@@ -23,5 +23,37 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    title: {
+      value: 'heading',
+      asHeading: {
+        size: Size.Medium,
+        tag: Tag.H2,
+      },
+      hint: 'Heading hint'
+    },
+    // inline: true,
+    errorMessage: 'error',
+    fieldId: 'UniqueID',
+    size: RadioSizeEnum.Small,
+    items: [
+      {
+        label: 'Employment Tribunal',
+        value: 'employment-tribunal',
+        hint: 'hint item 1'
+      },
+      {
+        label: 'Ministry of Defence',
+        value: 'ministry-of-defence',
+      },
+      {
+        label: 'Department for Transport',
+        value: 'department-for-transport',
+      },
+    ],
+    dividerOption: {
+      label: 'No, I will not be travelling to any of these countries',
+      value: 'no-travel',
+    },
+  },
 };
