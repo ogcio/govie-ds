4ec33a45e234d8123a9dbeba609ae4fe1a9755f4
Ramon Mulia <ramonmulia@gmail.com>
2024-10-21 09:28:53 -0300

    fixing schema and typescript error while building html/ds

--- packages/html/ds/src/list/list.schema.ts
+++ packages/html/ds/src/list/list.schema.ts
@@ -7,10 +7,20 @@ export enum TypeEnum {
   Number = 'number',
 }
 
-const linkPropsSchema = zod.object({
-  items: zod.array(zod.union([zod.string(), linkSchema])),
-  spaced: zod.boolean().optional(),
-  type: zod.nativeEnum(TypeEnum).optional(),
+export const linkPropsSchema = zod.object({
+  items: zod.array(zod.union([zod.string(), linkSchema]), {
+    description: 'Array of items that can be either strings or links.',
+  }),
+  spaced: zod
+    .boolean()
+    .optional()
+    .describe('Optional boolean to define spacing between items.'),
+  type: zod
+    .nativeEnum(TypeEnum)
+    .optional()
+    .describe(
+      'Optional enum to define the type of the list (bullet, number, none).',
+    ),
 });
 
 export type ListProps = zod.infer<typeof linkPropsSchema>;
