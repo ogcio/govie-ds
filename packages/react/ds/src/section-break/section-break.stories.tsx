ad4050aac3d08aa48e4b8ea0a045a930c754fbed
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-11-04 09:30:08 +0200

    add section break

--- packages/react/ds/src/section-break/section-break.stories.tsx
+++ packages/react/ds/src/section-break/section-break.stories.tsx
@@ -2,13 +2,33 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { SectionBreak } from './section-break.js';
 
 const meta = {
-  title: 'typography/SectionBreak',
+  title: 'Typography/SectionBreak',
   component: SectionBreak,
+  parameters: {
+    docs: {
+      description: {
+        docs: {
+          component:
+            'Section Break component to create a thematic break between sections of content.',
+        },
+      },
+    },
+  },
 } satisfies Meta<typeof SectionBreak>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  argTypes: {
+    size: {
+      control: 'radio',
+      options: ['sm', 'md', 'lg', 'xl'],
+      type: { name: 'string', required: false },
+      description: 'Specifies the size of the section break margin.',
+    },
+  },
+  args: {
+    size: 'md',
+  },
 };
