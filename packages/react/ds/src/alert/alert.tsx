f9c811c535920aa1cdd99b5dbd100ba17b91533e
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-11-13 09:37:37 +0200

    chromatic fix

--- packages/react/ds/src/alert/alert.tsx
+++ packages/react/ds/src/alert/alert.tsx
@@ -7,6 +7,7 @@ import { IconButton } from '../icon-button/icon-button.js';
 const alertVariants = tv({
   slots: {
     base: 'gi-alert-base',
+    baseDismissible: 'gi-alert-base-dismissible',
     container: 'gi-alert-container',
     heading: 'gi-alert-title',
     dismiss: 'gi-alert-dismiss',
@@ -15,15 +16,19 @@ const alertVariants = tv({
     variant: {
       info: {
         base: 'gi-alert-info',
+        baseDismissible: 'gi-alert-info',
       },
       danger: {
         base: 'gi-alert-danger',
+        baseDismissible: 'gi-alert-danger',
       },
       success: {
         base: 'gi-alert-success',
+        baseDismissible: 'gi-alert-success',
       },
       warning: {
         base: 'gi-alert-warning',
+        baseDismissible: 'gi-alert-success',
       },
     },
   },
@@ -64,15 +69,17 @@ const icon = ({ variant }: VariantProps<typeof alertVariants>) => {
 function Alert({ title, children, variant = 'info', dismissible }: AlertProps) {
   const [isDismissed, setIsDismissed] = useState(false);
 
-  const { base, heading, container, dismiss } = alertVariants({
+  const { base, heading, container, dismiss, baseDismissible } = alertVariants({
     variant,
   });
+
+  const baseVariant = dismissible ? baseDismissible : base;
 
   if (isDismissed) {
     return null;
   }
   return (
-    <div className={base()} role="alert">
+    <div className={baseVariant()} role="alert">
       <Icon icon={icon({ variant })} />
       <div className={container()}>
         <p className={heading()}>{title}</p>
@@ -82,6 +89,7 @@ function Alert({ title, children, variant = 'info', dismissible }: AlertProps) {
         <IconButton
           onClick={() => setIsDismissed(true)}
           className={dismiss()}
+          size="small"
           appearance="dark"
           variant="flat"
           icon={{ icon: 'close' }}
