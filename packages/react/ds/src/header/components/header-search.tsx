5c5c32165e2d38442bc834882a5749db2e3c784e
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-11-15 15:49:48 +0100

    fix: search box

--- packages/react/ds/src/header/components/header-search.tsx
+++ packages/react/ds/src/header/components/header-search.tsx
@@ -5,37 +5,31 @@ import { TextInput } from '../../text-input/text-input.js';
 function HeaderSearch({
   action,
   serverAction,
-  className,
   icon = 'search',
 }: {
   action?: string;
   serverAction?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-  className?: string;
   icon?: IconId;
 }) {
   const ActionType = action || serverAction;
   return (
-    <div
-      id="SearchContainer"
-      className={`gi-flex gi-h-0 gi-bg-gray-50 gi-overflow-hidden gi-px-4 xs:gi-px-8 ${className}`}
-    >
-      <div className="gi-w-fit gi-mx-auto gi-flex-col gi-my-8">
-        <form action={ActionType}>
-          <div className="gi-flex gi-items-end">
-            <TextInput
-              label={{ text: 'Enter search term', htmlFor: 'search' }}
-              name="search_query"
-              id="search"
-              type="text"
-            />
-            <IconButton
-              icon={{ icon, ariaLabel: 'Open' }}
-              size="medium"
-              className="gi-mb-4 gi-ml-1"
-            />
-          </div>
-        </form>
-      </div>
+    <div id="SearchContainer" className={`gi-hidden gi-bg-gray-50 gi-p-4`}>
+      <form action={ActionType}>
+        <div className="gi-flex gi-items-end gi-max-w-md gi-mx-auto">
+          <TextInput
+            label={{ text: 'Enter search term', htmlFor: 'search' }}
+            name="search_query"
+            id="search"
+            type="text"
+            className="gi-flex-auto"
+          />
+          <IconButton
+            icon={{ icon, ariaLabel: 'Open' }}
+            size="medium"
+            className="gi-mb-4 gi-ml-1 gi-flex-none"
+          />
+        </div>
+      </form>
     </div>
   );
 }
