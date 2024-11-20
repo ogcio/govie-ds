6836fef248a026d9637a412688a79b0c37249c31
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-11-20 17:14:35 +0100

    chore: remove unused fields

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
       >
         <Header {...headerProps} />
         {children}
-        <Footer links={footerLinks} />;
+        <Footer links={footerLinks} />
       </body>
     </html>
   );
