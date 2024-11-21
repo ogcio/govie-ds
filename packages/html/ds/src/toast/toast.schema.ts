2c24a3e0c2df943b46ab89c70edc3f73580b2645
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-11-21 09:57:08 +0200

    add base DS toast

---
+++ packages/html/ds/src/toast/toast.schema.ts
@@ -0,0 +1,54 @@
+import * as zod from 'zod';
+import { buttonSchema } from '../button/button-schema';
+import { linkSchema } from '../link/link.schema';
+
+export enum ToastVariant {
+  INFO = 'info',
+  DANGER = 'danger',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+}
+
+export const toastSchema = zod.object({
+  duration: zod
+    .number({ description: 'Specify the content in the toast component' })
+    .optional(),
+  position: zod
+    .object({
+      x: zod.union([
+        zod.literal('center'),
+        zod.literal('left'),
+        zod.literal('right'),
+      ]),
+      y: zod.union([
+        zod.literal('center'),
+        zod.literal('top'),
+        zod.literal('bottom'),
+      ]),
+    })
+    .describe('Set the duration of the toast appearing on screen')
+    .optional(),
+  trigger: buttonSchema
+    .optional()
+    .describe(
+      'If specified the toast will be triggered by the click event of this React Button Component',
+    ),
+  variant: zod
+    .nativeEnum(ToastVariant, { description: 'Toast variant' })
+    .optional(),
+  title: zod.string({
+    description: 'Specify the title of the toast component',
+  }),
+  description: zod
+    .string({ description: 'Specify the content in the toast component' })
+    .optional(),
+  action: linkSchema
+    .optional()
+    .describe('Specify a link for the toast component')
+    .optional(),
+  dismissible: zod
+    .boolean({ description: 'Specify if the toast is dismissible' })
+    .optional(),
+});
+
+export type ToastProps = zod.infer<typeof toastSchema>;
