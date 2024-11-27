ea9bee3fc0017c611c41148511df4c9e23ee2f6f
BogdanDanilescu <153850559+BogdanDanilescu@users.noreply.github.com>
2024-11-27 15:09:25 +0200

    feat: remove margins (#341)
    Co-authored-by: “BogdanDanilescu” <bogdan.danilescu@nearform.com>

--- packages/html/ds/src/paragraph/paragraph.stories.ts
+++ packages/html/ds/src/paragraph/paragraph.stories.ts
@@ -9,7 +9,9 @@ import {
   WhitespaceEnum,
 } from './paragraph.schema';
 
-const macro = { name: 'govieParagraph', html };
+const path = import.meta.url.split('/paragraph')[0];
+
+const macro = { name: 'govieParagraph', html, path };
 
 const Paragraph = renderComponent<ParagraphProps>(macro);
 
