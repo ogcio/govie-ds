cb0ae0744e440befcb2fd95defa1f74300c3d97e
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-12-05 15:47:04 +0100

    fix: tailwind typography configuration (#354)

--- packages/react/ds/tailwind.config.ts
+++ packages/react/ds/tailwind.config.ts
@@ -5,13 +5,17 @@ const config: Config = {
   prefix: 'gi-',
   content: ['./src/**/*.ts', './src/**/*.tsx'],
   theme: createTheme(),
-  plugins: [],
+  // eslint-disable-next-line unicorn/prefer-module
+  plugins: [require('@tailwindcss/typography')],
   safelist: [
+    'gi-prose',
+    'gi-not-prose',
+    'gi-max-w-none',
+    'gi-grid',
     { pattern: /grid-./ },
     { pattern: /col-./ },
     { pattern: /row-./ },
-    { pattern: /font-./ },
-    { pattern: /gap-./, variants: ['xs', 'sm', 'md', 'lg', 'xl', '2xl'] },
+    { pattern: /gap-./, variants: ['sm', 'md', 'lg', 'xl', '2xl'] },
   ],
 };
 
