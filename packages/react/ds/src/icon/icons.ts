638ed83cc50d570d01d679da82c98bef1dda0045
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2024-12-09 17:22:01 +0100

    fix: icon resources

---
+++ packages/react/ds/src/icon/icons.ts
@@ -0,0 +1,23 @@
+export const iconIds = [
+  'content_copy',
+  'edit',
+  'mic',
+  'send',
+  'thumb_down',
+  'thumb_up',
+  'open_in_new',
+  'attach_file',
+  'close',
+  'search',
+  'menu',
+  'home',
+  'logout',
+  'download',
+  'keyboard_arrow_down',
+  'info',
+  'check_circle',
+  'error',
+  'warning',
+] as const;
+
+export const Icons = [...iconIds];
