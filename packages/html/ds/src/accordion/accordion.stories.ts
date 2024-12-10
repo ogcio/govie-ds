e34a3036e2549cabdcb1578a7a8df1b98b9264cc
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-12-10 15:53:18 +0200

    add stories for iconStart

--- packages/html/ds/src/accordion/accordion.stories.ts
+++ packages/html/ds/src/accordion/accordion.stories.ts
@@ -52,3 +52,31 @@ export const Default: Story = {
     ],
   },
 };
+
+export const WithIconStart: Story = {
+  args: {
+    iconStart: true,
+    items: [
+      {
+        label: 'Label 1',
+        content: `<h2 class="gi-heading-xs">This is the content heading</h2><p class="gi-paragraph-md gi-text-start gi-whitespace-normal ">This is a content paragraph paragraph</p>`,
+      },
+      {
+        label: 'Label 2',
+        content: `<h2 class="gi-heading-xs">This is the content heading</h2><a href="#" class="gi-link gi-link-md">Link</a><p class="gi-paragraph-md gi-text-start gi-whitespace-normal ">  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+          minus eveniet ex officiis accusantium sint eius deleniti cumque? Iste
+          voluptatum omnis harum quaerat eius praesentium a at perferendis
+          quisquam hic.</p>`,
+      },
+      {
+        label: 'Label 3',
+        content: `<h2 class="gi-heading-xs">This is the content heading</h2><button data-testid="govieButton-default-primary-medium-notDisabled" data-element="button-container" data-module="gieds-button" class="gi-btn gi-btn-primary gi-btn-regular">Button</button>`,
+      },
+      {
+        disabled: true,
+        label: 'Label 4',
+        content: `<h2 class="gi-heading-xs">This is the content heading</h2><button data-testid="govieButton-default-primary-medium-notDisabled" data-element="button-container" data-module="gieds-button" class="gi-btn gi-btn-primary gi-btn-regular">Button</button>`,
+      },
+    ],
+  },
+};
--- packages/react/ds/src/accordion/accordion.stories.tsx
+++ packages/react/ds/src/accordion/accordion.stories.tsx
@@ -56,3 +56,40 @@ export const Default = {
     </Accordion>
   ),
 };
+
+export const WithIconStart = {
+  render: () => (
+    <Accordion iconStart={true}>
+      <AccordionItem label="Label1">
+        <Heading size="xs" as="h2">
+          This is the content Heading
+        </Heading>
+        <Paragraph>This is a content paragraph paragraph</Paragraph>
+      </AccordionItem>
+      <AccordionItem label="Label2">
+        <Heading size="xs" as="h2">
+          This is the content Heading
+        </Heading>
+        <Link href="#">This is a link</Link>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+          minus eveniet ex officiis accusantium sint eius deleniti cumque? Iste
+          voluptatum omnis harum quaerat eius praesentium a at perferendis
+          quisquam hic.
+        </Paragraph>
+      </AccordionItem>
+      <AccordionItem label="Label3">
+        <Heading size="xs" as="h2">
+          This is the content Heading
+        </Heading>
+        <Button>This is a button</Button>
+      </AccordionItem>
+      <AccordionItem disabled label="Disabled Accordion">
+        <Heading size="xs" as="h2">
+          This is the content Heading
+        </Heading>
+        <Button>This is a button</Button>
+      </AccordionItem>
+    </Accordion>
+  ),
+};
