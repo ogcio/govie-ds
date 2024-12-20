644769338840b7511771e4b90a4a7ef5fa7f53ab
“BogdanDanilescu” <bogdan.danilescu@nearform.com>
2024-12-20 13:36:02 +0200

    update docs

--- apps/docs/src/components/document/common/mdx.tsx
+++ apps/docs/src/components/document/common/mdx.tsx
@@ -50,6 +50,8 @@ import {
   TableBody,
   Link,
   Tooltip,
+  DropdownItem,
+  Form,
 } from '@govie-ds/react';
 import { MDXComponents } from 'mdx/types';
 import { useMDXComponent } from 'next-contentlayer/hooks';
@@ -125,6 +127,10 @@ const standardComponents: MDXComponents = {
 };
 
 const documentComponents: MDXComponents = {
+  DropdownItem: (props) => (
+    <DropdownItem {...props}>{props.children}</DropdownItem>
+  ),
+  Form: (props) => <Form {...props}>{props.children}</Form>,
   Image: (props) => <DocumentImage {...props} />,
   ColorPrimitives: () => <ColorPrimitives />,
   FontFamilyTable: () => <FontFamilyTable />,
@@ -179,7 +185,7 @@ const documentComponents: MDXComponents = {
   Modal: (props) => <Modal {...props}>{props.children}</Modal>,
   CookieBanner: (props) => <CookieBanner {...props} />,
   List: (props) => <List {...props} />,
-  ComboBox: (props) => <Combobox {...props} />,
+  ComboBox: (props) => <Combobox {...props}>{props.children}</Combobox>,
   Chip: (props) => <Chip {...props} />,
   IconGridLayout: (props) => <IconGridLayout {...props} />,
   TextInput: (props) => <TextInput {...props} />,
