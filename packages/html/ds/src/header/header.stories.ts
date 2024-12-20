2f34f28c678d1e3d65ef6c53d2d67393327261e2
Ramon Mulia <ramonmulia@gmail.com>
2024-12-20 07:09:03 -0300

    removing languages from default story

--- packages/html/ds/src/header/header.stories.ts
+++ packages/html/ds/src/header/header.stories.ts
@@ -150,16 +150,6 @@ export const Default: Story = {
         label: 'Services',
       },
     ],
-    languages: [
-      {
-        href: '#',
-        label: 'Gaeilge',
-      },
-      {
-        href: '#',
-        label: 'English',
-      },
-    ],
   },
 };
 
