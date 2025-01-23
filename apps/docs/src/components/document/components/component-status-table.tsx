633d97b0a8becb4fcf67eed264636c1855c43245
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2025-01-23 17:45:39 +0100

    docs: review components status (#404)

--- apps/docs/src/components/document/components/component-status-table.tsx
+++ apps/docs/src/components/document/components/component-status-table.tsx
@@ -1,5 +1,14 @@
 'use client';
-import { IconButton, Paragraph, Tag, TagType } from '@govie-ds/react';
+import {
+  IconButton,
+  Paragraph,
+  Table,
+  TableBody,
+  TableData,
+  TableRow,
+  Tag,
+  TagType,
+} from '@govie-ds/react';
 import { Fragment } from 'react';
 import { ComponentStatus, getComponents } from '@/lib/components';
 
@@ -61,9 +70,6 @@ export function ComponentStatusBlock({ componentId }: { componentId: string }) {
       const figmaPlatform = component.statuses.find(
         (platformStatus) => platformStatus.platform.id === 'figma',
       );
-      const localPlatform = component.statuses.find(
-        (platformStatus) => platformStatus.platform.id === 'local',
-      );
       const globalPlatform = component.statuses.find(
         (platformStatus) => platformStatus.platform.id === 'global',
       );
@@ -77,10 +83,6 @@ export function ComponentStatusBlock({ componentId }: { componentId: string }) {
         figma: {
           status: figmaPlatform?.status ?? 'considering',
           href: figmaPlatform?.platform?.href,
-        },
-        local: {
-          status: localPlatform?.status ?? 'considering',
-          href: localPlatform?.platform?.href,
         },
         global: {
           status: globalPlatform?.status ?? 'considering',
@@ -109,15 +111,6 @@ export function ComponentStatusBlock({ componentId }: { componentId: string }) {
             <ComponentStatusPill
               status={componentStatus.figma.status}
               href={componentStatus.figma.href}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td className="p-2">Local HTML</td>
-          <td>
-            <ComponentStatusPill
-              status={componentStatus.local.status}
-              href={componentStatus.local.href}
             />
           </td>
         </tr>
@@ -151,9 +144,6 @@ export function ComponentStatusTable() {
     const figmaPlatform = component.statuses.find(
       (platformStatus) => platformStatus.platform.id === 'figma',
     );
-    const localPlatform = component.statuses.find(
-      (platformStatus) => platformStatus.platform.id === 'local',
-    );
     const globalPlatform = component.statuses.find(
       (platformStatus) => platformStatus.platform.id === 'global',
     );
@@ -168,10 +158,6 @@ export function ComponentStatusTable() {
         status: figmaPlatform?.status ?? 'considering',
         href: figmaPlatform?.platform?.href,
       },
-      local: {
-        status: localPlatform?.status ?? 'stable',
-        href: localPlatform?.platform?.href,
-      },
       global: {
         status: globalPlatform?.status ?? 'considering',
         href: globalPlatform?.platform?.href,
@@ -185,19 +171,51 @@ export function ComponentStatusTable() {
 
   return (
     <div>
+      <div className="max-w-prose overflow-auto gi-not-prose">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableData className="border-none">
+                <Tag text="alpha" type="warning" />
+              </TableData>
+              <TableData className="border-none text-wrap">
+                The design and/or the implementation of the component is not
+                definitive yet. The component can be subject to major
+                refactoring.
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData className="border-none">
+                <Tag text="beta" type="info" />
+              </TableData>
+              <TableData className="border-none text-wrap">
+                The component has an approved design on Figma and is
+                implemented. There can be changes due to feedback from early
+                adopters or minor refactoring.
+              </TableData>
+            </TableRow>
+            <TableRow>
+              <TableData className="border-none">
+                <Tag text="stable" type="success" />
+              </TableData>
+              <TableData className="border-none text-wrap">
+                The component will not have design changes or major refactoring.
+              </TableData>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+
       <Paragraph>
         There are currently <strong>{components.length}</strong> components
         under consideration for the design system.
       </Paragraph>
-      <div className="grid grid-cols-2 lg:grid-cols-5">
+      <div className="grid grid-cols-2 lg:grid-cols-4">
         <div className="hidden lg:block bg-gray-50 p-2 text-sm text-gray-600">
           Component
         </div>
         <div className="hidden lg:block bg-gray-50 p-2 text-sm text-gray-600">
           Figma Library
-        </div>
-        <div className="hidden lg:block bg-gray-50 p-2 text-sm text-gray-600">
-          Local HTML
         </div>
         <div className="hidden lg:block bg-gray-50 p-2 text-sm text-gray-600">
           Global HTML
@@ -209,7 +227,7 @@ export function ComponentStatusTable() {
         {componentStatuses.map((componentStatus) => {
           return (
             <Fragment key={componentStatus.id}>
-              <div className="row-span-4 lg:row-span-1 mb-4 lg:mb-0 w-32 lg:w-full p-2">
+              <div className="row-span-3 lg:row-span-1 mb-4 lg:mb-0 w-32 lg:w-full p-2">
                 {componentStatus.name}
               </div>
               <div className="flex p-2">
@@ -217,13 +235,6 @@ export function ComponentStatusTable() {
                 <ComponentStatusPill
                   status={componentStatus.figma.status}
                   href={componentStatus.figma.href}
-                />
-              </div>
-              <div className="flex p-2">
-                <div className="w-32 block lg:hidden">Local HTML</div>
-                <ComponentStatusPill
-                  status={componentStatus.local.status}
-                  href={componentStatus.local.href}
                 />
               </div>
               <div className="flex p-2">
