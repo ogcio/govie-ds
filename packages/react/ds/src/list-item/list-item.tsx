b1b64b48673c53b5b35e3a109978223eb1066986
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2025-02-20 17:57:50 +0100

    feat: add list item component

---
+++ packages/react/ds/src/list-item/list-item.tsx
@@ -0,0 +1,25 @@
+import { cn } from '../cn.js';
+import Anchor from '../primitives/anchor.js';
+
+export type ListItemProps = {
+  label: string;
+  action?: () => void;
+  href?: string;
+  external?: boolean;
+  bold?: boolean;
+};
+
+export function ListItem({ label, href, external, bold }: ListItemProps) {
+  return (
+    <Anchor
+      aria-label={label}
+      href={href}
+      className="gi-list-item"
+      external={external}
+    >
+      <span className={cn('gi-text-sm', 'gi-ml-1', { 'gi-font-bold': bold })}>
+        {label}
+      </span>
+    </Anchor>
+  );
+}
