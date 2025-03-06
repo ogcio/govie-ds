9c5c3816f13c5660c8918749e1034b333de37af6
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2025-03-06 18:18:40 +0100

    chore: use semantic tokens for header colours

--- packages/design/theme-builder/src/validation/schema/semantic/semantic-schema.ts
+++ packages/design/theme-builder/src/validation/schema/semantic/semantic-schema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
+import { surfaceSchema } from './surface-schema.js';
 import { typographySchema } from './typography-schema.js';
 
 export const semanticSchema = z
   .object(
     {
       typography: typographySchema,
+      surface: surfaceSchema,
     },
     { required_error: 'semantic is required.' },
   )
