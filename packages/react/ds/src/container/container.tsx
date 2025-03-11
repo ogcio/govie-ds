bd372e25b67a949b18e8442d0f24c89563826fd2
Ramon Mulia <ramonmulia@gmail.com>
2025-03-11 23:51:55 +0800

    fixes and adding insetTop and insetBottom

--- packages/react/ds/src/container/container.tsx
+++ packages/react/ds/src/container/container.tsx
@@ -1,17 +1,37 @@
 import { cn } from '../cn.js';
 
+export const ContainerInsetSizeEnum = {
+  None: 'none',
+  Medium: 'md',
+  Large: 'lg',
+  ExtraLarge: 'xl',
+} as const;
+
+export type ContainerInsetSizeType =
+  (typeof ContainerInsetSizeEnum)[keyof typeof ContainerInsetSizeEnum];
+
 type ContainerProps = {
   children: React.ReactNode;
   id?: string;
-  inset?: boolean;
+  insetTop?: ContainerInsetSizeType;
+  insetBottom?: ContainerInsetSizeType;
 };
 
-export function Container({ children, id, inset }: ContainerProps) {
+export function Container({
+  children,
+  id,
+  insetBottom,
+  insetTop,
+}: ContainerProps) {
+  const hasInset = insetTop || insetBottom;
   return (
     <div
       data-testid="govie-container"
-      className={cn('gi-layout-container', {
-        'gi-layout-container-inset': inset,
+      data-inset-top={insetTop}
+      data-inset-bottom={insetBottom}
+      className={cn({
+        'gi-layout-container': !hasInset,
+        'gi-layout-container-inset': hasInset,
       })}
       id={id}
     >
