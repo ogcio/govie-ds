ea1d4c608399f4e5aa9e53d04761342523c9113d
hamza14khan <hamzaubit14@gmail.com>
2025-03-24 08:28:08 +0000

    fix: update table html stories

--- packages/html/ds/src/table/table.schema.ts
+++ packages/html/ds/src/table/table.schema.ts
@@ -13,6 +13,12 @@ export const LayoutVariant = {
   Fixed: 'fixed',
 } as const;
 
+export const Alignment = {
+  Left: 'left',
+  Center: 'center',
+  Right: 'right',
+} as const;
+
 export const ariaSchema = zod.record(
   zod.enum(validAriaProps, {
     description: 'Valid ARIA attributes key',
@@ -24,31 +30,36 @@ export const ariaSchema = zod.record(
 );
 
 export const tableSchema = zod.object({
-  captionText: zod
-    .string({
-      description:
-        'Text for the table caption, providing a brief description of the table contents.',
-    })
-    .optional(),
+  captionText: zod.string().optional(),
   headers: zod
-    .array(zod.string(), {
-      description:
-        'An array of strings representing column headers for the table.',
-    })
+    .array(
+      zod.object({
+        text: zod.string().optional(),
+        align: zod.enum(getEnumValues(Alignment)).optional(),
+      }),
+      {
+        description:
+          'Array of headers, each with "text" and optionally "align"',
+      },
+    )
     .optional(),
   layout: zod.enum(getEnumValues(LayoutVariant)).optional(),
   rows: zod
-    .array(zod.any(), {
-      description:
-        'An array representing rows of the table. Each row can contain HTML elements, text, or any other type.',
-    })
+    .array(
+      zod.array(
+        zod.object({
+          content: zod.any(),
+          align: zod.enum(getEnumValues(Alignment)).optional(),
+        }),
+      ),
+      {
+        description:
+          'Rows array with cells containing "content" and optional "align"',
+      },
+    )
     .optional(),
-  aria: ariaSchema.describe('Defines the aria attributes').optional(),
-  dataTestid: zod
-    .string({
-      description: 'Test id for the component.',
-    })
-    .optional(),
+  aria: ariaSchema.optional(),
+  dataTestid: zod.string().optional(),
 });
 
 export type TableProps = zod.infer<typeof tableSchema>;
