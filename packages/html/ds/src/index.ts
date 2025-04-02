b789fd052b67c1cb946da87ae406a4dbab03b57c
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2025-04-02 17:39:31 +0200

    fix: order issue

--- packages/html/ds/src/index.ts
+++ packages/html/ds/src/index.ts
@@ -1,7 +1,3 @@
-import { initAccordion } from './accordion/accordion.js';
-import { initAlert } from './alert/alert.js';
-import { initCheckboxes } from './checkbox/checkbox.js';
-import { initComboBox } from './combo-box/combo-box.js';
 import { destroyAllInstances } from './common/instances.js';
 import { initCookieBanner } from './cookie-banner/cookie-banner.js';
 import { initDrawer } from './drawer/drawer.js';
@@ -14,6 +10,12 @@ import { initTextarea } from './textarea/textarea.js';
 import { initToast } from './toast/toast.js';
 import { initTooltip } from './tooltip/tooltip.js';
 import { initUtilities } from './utils/utilities.js';
+import { initAlert } from './alert/alert.js';
+import { initCheckboxes } from './checkbox/checkbox.js';
+import { initComboBox } from './combo-box/combo-box.js';
+
+// Don't reorder the following import
+import { initAccordion } from './accordion/accordion.js';
 
 export * as properties from './dist/properties.js';
 
