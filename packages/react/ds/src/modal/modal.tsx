fcefd733b29532fb957a25992f3985c2492b5fb7
Ramon Mulia <ramonmulia@gmail.com>
2025-04-03 11:06:31 -0300

    drawer width fixes

--- packages/react/ds/src/modal/modal.tsx
+++ packages/react/ds/src/modal/modal.tsx
@@ -148,8 +148,9 @@ export const ModalWrapper = ({
         data-testid="modal-container"
         data-size={size}
         className={cn(
-          'gi-modal-container',
+          'gi-modal-container-control',
           {
+            'gi-modal-container': !className,
             'gi-modal-container-center': position === 'center',
             'gi-modal-container-left': position === 'left',
             'gi-modal-container-right': position === 'right',
@@ -195,7 +196,15 @@ export const ModalBody = ({
   children: ReactNode;
   className?: string;
 }) => (
-  <div id="gi-modal-body" className={cn('gi-modal-body', className)}>
+  <div
+    id="gi-modal-body"
+    className={cn(
+      {
+        'gi-modal-body': !className,
+      },
+      className,
+    )}
+  >
     {children}
   </div>
 );
