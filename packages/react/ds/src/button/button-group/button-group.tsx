b8f9932dbfaa37f9fef8d3333e13f762d6509396
hamza14khan <hamzaubit14@gmail.com>
2025-04-11 05:25:18 +0100

    feat: update button group with button group item exposed

--- packages/react/ds/src/button/button-group/button-group.tsx
+++ packages/react/ds/src/button/button-group/button-group.tsx
@@ -1,62 +1,79 @@
 'use client';
 import React, { useState } from 'react';
-import { FormField } from '../../forms/form-field.js';
 import { Button } from '../button.js';
 import { ButtonSize } from '../types.js';
-type Option = {
-  label: string;
-  value: string;
-};
 
-type ButtonGroupProps = {
-  options: Option[];
+type ButtonGroupContextType = {
+  selectedValue?: string;
+  setSelectedValue: (value: string) => void;
   name: string;
-  label: string;
-  hint?: string;
-  size?: ButtonSize;
+  size: ButtonSize;
   onChange?: (value: string) => void;
-  defaultValue?: string;
 };
 
-export const ButtonGroup: React.FC<ButtonGroupProps> = ({
-  options,
-  name,
-  label,
-  hint,
-  size,
-  onChange,
-  defaultValue,
-}) => {
-  const [selected, setSelected] = useState<string | undefined>(defaultValue);
+const ButtonGroupContext = React.createContext<
+  ButtonGroupContextType | undefined
+>(undefined);
 
-  const handleClick = (value: string) => {
-    setSelected(value);
+type ButtonGroupItemProps = {
+  value: string;
+  children: React.ReactNode;
+};
+
+export const ButtonGroupItem: React.FC<ButtonGroupItemProps> = ({
+  value,
+  children,
+}) => {
+  const context = React.useContext(ButtonGroupContext);
+
+  if (!context) {
+    throw new Error('ButtonGroupItem must be used within a ButtonGroup');
+  }
+
+  const { selectedValue, setSelectedValue, name, size, onChange } = context;
+
+  const handleClick = () => {
+    setSelectedValue(value);
     onChange?.(value);
   };
 
   return (
-    <FormField
-      className="gi-w-full"
-      label={{
-        text: label,
-        htmlFor: name,
-      }}
-      hint={hint ? { text: hint } : undefined}
+    <Button
+      variant={selectedValue === value ? 'primary' : 'secondary'}
+      size={size}
+      appearance="dark"
+      onClick={handleClick}
+      id={`${name}-${value}`}
     >
-      <div className="gi-flex gi-flex-wrap gi-gap-2 gi-pt-2 gi-justify-between">
-        {options.map((option) => (
-          <Button
-            key={option.value}
-            variant={selected === option.value ? 'primary' : 'secondary'}
-            size={size || 'medium'}
-            appearance="dark"
-            onClick={() => handleClick(option.value)}
-            id={`${name}-${option.value}`}
-          >
-            {option.label}
-          </Button>
-        ))}
-      </div>
-    </FormField>
+      {children}
+    </Button>
+  );
+};
+
+type ButtonGroupProps = {
+  name: string;
+  size?: ButtonSize;
+  onChange?: (value: string) => void;
+  defaultValue?: string;
+  children?: React.ReactNode;
+};
+
+export const ButtonGroup: React.FC<ButtonGroupProps> = ({
+  name,
+  size = 'medium',
+  onChange,
+  defaultValue,
+  children,
+}) => {
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    defaultValue,
+  );
+
+  return (
+    <ButtonGroupContext.Provider
+      value={{ selectedValue, setSelectedValue, name, size, onChange }}
+    >
+      <div className="gi-btn-group">{children}</div>
+    </ButtonGroupContext.Provider>
   );
 };
