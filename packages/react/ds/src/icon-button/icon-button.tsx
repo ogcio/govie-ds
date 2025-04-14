fa6ac7d69356b0ea0aa0643dc0e8fbe826d439df
Ramon Mulia <ramonmulia@gmail.com>
2025-04-14 07:42:59 -0300

    fix: adding not-allowed cursor in disabled buttons

--- packages/react/ds/src/icon-button/icon-button.tsx
+++ packages/react/ds/src/icon-button/icon-button.tsx
@@ -1,9 +1,12 @@
 'use client';
+import { forwardRef } from 'react';
 import {
   isButtonDisabled,
   getVariantAppearanceClass,
+  getButtonIconSizeClass,
 } from '../button/helpers.js';
-import { ButtonProps, ButtonSize } from '../button/types.js';
+import { ButtonProps } from '../button/types.js';
+import { cn } from '../cn.js';
 import { Icon, IconProps } from '../icon/icon.js';
 
 export type IconButtonProps = Omit<ButtonProps, 'children'> & {
@@ -11,37 +14,32 @@ export type IconButtonProps = Omit<ButtonProps, 'children'> & {
   className?: string;
 };
 
-const getSizeClass = (size?: ButtonSize) => {
-  let classes;
-  if (size === 'small') {
-    classes = 'gi-icon-btn-small';
-  } else if (size === 'large') {
-    classes = 'gi-icon-btn-large';
-  } else {
-    classes = 'gi-icon-btn-regular';
-  }
-  return classes;
-};
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  (
+    { icon, variant, appearance, size, disabled, onClick, className = '' },
+    ref,
+  ) => {
+    const iconSize = size === 'small' ? 'sm' : 'md';
+    return (
+      <button
+        ref={ref}
+        aria-disabled={disabled}
+        disabled={disabled}
+        role="button"
+        data-testid={`govieIconButton-${appearance}-${variant}-${size}-${disabled}`}
+        onClick={onClick}
+        className={cn(
+          'gi-btn',
+          getVariantAppearanceClass({ disabled, variant, appearance }),
+          getButtonIconSizeClass(size),
+          isButtonDisabled({ disabled, variant, appearance }),
+          className,
+        )}
+      >
+        <Icon size={iconSize} {...icon} />
+      </button>
+    );
+  },
+);
 
-export const IconButton = ({
-  icon,
-  variant,
-  appearance,
-  size,
-  disabled,
-  onClick,
-  className = '',
-}: IconButtonProps) => {
-  const iconSize = size === 'small' ? 'sm' : 'md';
-  return (
-    <button
-      aria-disabled={disabled}
-      role="button"
-      data-testid={`govieIconButton-${appearance}-${variant}-${size}-${disabled}`}
-      onClick={onClick}
-      className={`gi-btn ${getVariantAppearanceClass({ disabled, variant, appearance })} ${getSizeClass(size)} ${isButtonDisabled({ disabled, variant, appearance })} ${className}`}
-    >
-      <Icon size={iconSize} {...icon} />
-    </button>
-  );
-};
+IconButton.displayName = 'IconButton';
