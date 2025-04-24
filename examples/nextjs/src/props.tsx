bc675ab1526c37d1472d91f7c53867ea9948dd09
Ramon Mulia <ramonmulia@gmail.com>
2025-04-24 07:12:40 -0300

    adding examples vertical stepper and fixing CookieBanner example

--- examples/nextjs/src/props.tsx
+++ examples/nextjs/src/props.tsx
@@ -1,4 +1,4 @@
-import { Heading, Paragraph, Button, Link } from "@govie-ds/react";
+import { Heading, Paragraph, Button, Link } from '@govie-ds/react';
 
 export const CookieBannerProps = {
   children: (
@@ -13,167 +13,145 @@ export const CookieBannerProps = {
       </Paragraph>
     </>
   ),
-  accept: {
-    children: (
-      <>
-        <Heading as="h3">Accepted Title</Heading>
-        <Paragraph>
-          You’ve accepted analytics cookies. You can change your cookie settings
-          at any time.
-        </Paragraph>
-      </>
-    ),
-    triggerButton: <Button>Accept cookies</Button>,
-  },
-  reject: {
-    children: (
-      <>
-        <Heading as="h3">Rejected Title</Heading>
-        <Paragraph>
-          You’ve rejected analytics cookies. You can change your cookie settings
-          at any time.
-        </Paragraph>
-      </>
-    ),
-    triggerButton: <Button>Reject cookies</Button>,
-  },
+  accept: <Button>Accept cookies</Button>,
+  reject: <Button>Reject cookies</Button>,
   cookieLink: <Link href="#">See Cookies</Link>,
-  dismissButton: <Button>Hide this message</Button>,
+  showConsent: true,
 };
 
 export const ComboBoxProps = {
   organisationOptions: [
     {
-      label: "An Bord Pleanála",
-      value: "15431907-an-bord-pleanala",
+      label: 'An Bord Pleanála',
+      value: '15431907-an-bord-pleanala',
     },
     {
-      label: "An Garda Síochána",
-      value: "an-garda-siochana",
+      label: 'An Garda Síochána',
+      value: 'an-garda-siochana',
     },
     {
-      label: "Bord Bia (Irish Food Board)",
-      value: "14061907-bord-bia-irish-food-board",
+      label: 'Bord Bia (Irish Food Board)',
+      value: '14061907-bord-bia-irish-food-board',
     },
     {
-      label: "Capital Works Management Framework",
-      value: "7c2ea-capital-works-management-framework",
+      label: 'Capital Works Management Framework',
+      value: '7c2ea-capital-works-management-framework',
     },
     {
-      label: "Carlow County Council",
-      value: "carlow-county-council",
+      label: 'Carlow County Council',
+      value: 'carlow-county-council',
     },
     {
-      label: "Cavan County Council",
-      value: "cavan-county-council",
+      label: 'Cavan County Council',
+      value: 'cavan-county-council',
     },
     {
-      label: "Central Bank of Ireland",
-      value: "10032007-central-bank-of-ireland",
+      label: 'Central Bank of Ireland',
+      value: '10032007-central-bank-of-ireland',
     },
   ],
   categoryOptions: [
     {
-      label: "Agencies",
-      value: "agencies",
+      label: 'Agencies',
+      value: 'agencies',
     },
     {
-      label: "Aquaculture licences",
-      value: "aquaculture_licence",
+      label: 'Aquaculture licences',
+      value: 'aquaculture_licence',
     },
     {
-      label: "Biographies",
-      value: "biographies",
+      label: 'Biographies',
+      value: 'biographies',
     },
     {
-      label: "Campaigns",
-      value: "campaigns",
+      label: 'Campaigns',
+      value: 'campaigns',
     },
     {
-      label: "Circulars",
-      value: "circulars",
+      label: 'Circulars',
+      value: 'circulars',
     },
     {
-      label: "Collections",
-      value: "collections",
+      label: 'Collections',
+      value: 'collections',
     },
     {
-      label: "Consultations",
-      value: "consultations",
+      label: 'Consultations',
+      value: 'consultations',
     },
     {
-      label: "Coroner cases",
-      value: "coroner_cases",
+      label: 'Coroner cases',
+      value: 'coroner_cases',
     },
     {
-      label: "Departments",
-      value: "departments",
+      label: 'Departments',
+      value: 'departments',
     },
     {
-      label: "Forms",
-      value: "forms",
+      label: 'Forms',
+      value: 'forms',
     },
     {
-      label: "IGEES Publications",
-      value: "igees_publication",
+      label: 'IGEES Publications',
+      value: 'igees_publication',
     },
     {
-      label: "Local authorities",
-      value: "local_authorities",
+      label: 'Local authorities',
+      value: 'local_authorities',
     },
     {
-      label: "News",
-      value: "news",
+      label: 'News',
+      value: 'news',
     },
   ],
   topicOptions: [
     {
-      label: "Brexit",
-      value: "brexit",
+      label: 'Brexit',
+      value: 'brexit',
     },
     {
-      label: "Business and industry",
-      value: "business-and-industry",
+      label: 'Business and industry',
+      value: 'business-and-industry',
     },
     {
-      label: "COVID-19",
-      value: "covid-19",
+      label: 'COVID-19',
+      value: 'covid-19',
     },
     {
-      label: "Corporate information",
-      value: "corporate-information",
+      label: 'Corporate information',
+      value: 'corporate-information',
     },
     {
-      label: "Crime, justice and law",
-      value: "crime-justice-and-law",
+      label: 'Crime, justice and law',
+      value: 'crime-justice-and-law',
     },
     {
-      label: "Defence and armed forces",
-      value: "defence-and-armed-force",
+      label: 'Defence and armed forces',
+      value: 'defence-and-armed-force',
     },
     {
-      label: "Education, training and skills",
-      value: "education-training-and-skills",
+      label: 'Education, training and skills',
+      value: 'education-training-and-skills',
     },
     {
-      label: "News",
-      value: "news",
+      label: 'News',
+      value: 'news',
     },
     {
-      label: "Organisation information",
-      value: "organisation_information",
+      label: 'Organisation information',
+      value: 'organisation_information',
     },
     {
-      label: "Policies",
-      value: "policies",
+      label: 'Policies',
+      value: 'policies',
     },
     {
-      label: "Policy information",
-      value: "policy_information",
+      label: 'Policy information',
+      value: 'policy_information',
     },
     {
-      label: "Press releases",
-      value: "press_releases",
+      label: 'Press releases',
+      value: 'press_releases',
     },
   ],
 };
