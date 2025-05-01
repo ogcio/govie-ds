a80447fb23aabbe77bcf58fedc3cb9b76de396da
Ramon Mulia <ramonmulia@gmail.com>
2025-05-01 09:00:41 -0300

    updating stack

--- packages/html/ds/src/stack/stack.stories.ts
+++ packages/html/ds/src/stack/stack.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { beautifyHtmlNode } from '../storybook/storybook';
 import { StackProps } from './types';
+import { createButton } from '../helpers/buttons';
+import { createLink } from '../helpers/links';
 
 const meta: Meta<StackProps> = {
   title: 'layout/Stack',
@@ -220,6 +222,12 @@ const children2 = [
   '<div>Item 3</div>',
 ];
 
+const children3 = [
+  createButton({ content: 'Button 1' }),
+  createLink({ href: '#', content: 'Link 1' }),
+  createButton({ content: 'Button 2' }),
+];
+
 export const Default: Story = {
   args: {
     direction: 'column',
@@ -281,4 +289,41 @@ export const CenterDistribution: Story = {
     gap: 4,
   },
   render: (arguments_) => createElement(arguments_, children2),
+};
+
+export const WithComponents: Story = {
+  args: {
+    direction: { sm: 'row', md: 'column' },
+    itemsAlignment: 'center',
+    itemsDistribution: 'around',
+    gap: 4,
+    fixedHeight: '300px',
+  },
+  render: (arguments_) => createElement(arguments_, children3),
+};
+
+export const NestedStack: Story = {
+  render: () => `
+    <div class="gi-flex gi-w-full gi-justify-start gi-items-start gi-flex-col gi-gap-5 gi-flex-nowrap" role="region" aria-label="stack1" style="height: 100%;">
+    <div class="gi-flex gi-w-full gi-justify-start gi-items-start gi-flex-row gi-flex-nowrap" role="region" aria-label="stack2" data-testid="govie-stack-item-0" style="height: 100%;">
+        <div class="gi-h-[80px] gi-w-full gi-bg-gray-200" data-testid="govie-stack-item-0"></div>
+    </div>
+    <div class="gi-flex gi-w-full gi-justify-start gi-items-start gi-flex-row gi-gap-5 gi-flex-nowrap" role="region" aria-label="stack3" data-testid="govie-stack-item-1" style="height: 100%;">
+        <div class="gi-flex gi-w-full gi-justify-start gi-items-start gi-flex-col gi-gap-5 gi-flex-nowrap" role="region" aria-label="stack31" data-testid="govie-stack-item-0" style="height: 100%;">
+          <div class="gi-h-[200px] gi-w-full gi-bg-gray-200" data-testid="govie-stack-item-0"></div>
+        </div>
+        <div class="gi-flex gi-w-full gi-justify-start gi-items-start gi-flex-row gi-gap-5 gi-flex-wrap" role="region" aria-label="stack32" data-testid="govie-stack-item-1" style="height: 100%;">
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-0"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-1"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-2"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-3"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-4"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-5"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-6"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-7"></div>
+          <div class="gi-h-[50px] gi-w-[100px] gi-bg-gray-200" data-testid="govie-stack-item-8"></div>
+        </div>
+    </div>
+  </div>
+  `,
 };
