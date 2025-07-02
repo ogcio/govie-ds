e87b2d72c9c7ec6f6dd1af819de22bd0f131feef
Hamza Khan <164040832+hamza14khan@users.noreply.github.com>
2025-07-02 11:14:39 +0100

    fix: update react and html details component (#607)

--- packages/react/ds/src/details/details.tsx
+++ packages/react/ds/src/details/details.tsx
@@ -1,31 +1,52 @@
+'use client';
+
+import { useState, useRef, useEffect } from 'react';
+import { Icon } from '../icon/icon.js';
+
 export type DetailsProps = {
   label: string;
 } & React.DetailsHTMLAttributes<HTMLDetailsElement>;
 
-export const Details = ({ label, name, children, open }: DetailsProps) => {
+export const Details = ({ label, name, children, ...props }: DetailsProps) => {
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (detailsRef.current?.open !== isOpen) {
+      setIsOpen(detailsRef.current?.open ?? false);
+    }
+  }, []);
+
+  const handleToggle = () => {
+    setIsOpen(detailsRef.current?.open ?? false);
+  };
+
   return (
     <details
+      ref={detailsRef}
+      onToggle={handleToggle}
       data-testid="govie-details"
       className="gi-details"
       data-module="gi-details"
-      open={open}
       name={name}
-      aria-expanded={open ? 'true' : 'false'}
+      aria-expanded={isOpen ? 'true' : 'false'}
       aria-details="details-content"
+      {...props}
     >
       <summary
         className="gi-details-summary"
         data-testid="govie-details-summary"
         role="button"
         aria-controls="details-content"
-        aria-expanded={open ? 'true' : 'false'}
+        aria-expanded={isOpen ? 'true' : 'false'}
       >
+        <Icon icon={isOpen ? 'keyboard_arrow_up' : 'keyboard_arrow_down'} />
         <span className="gi-details-summary-text">{label}</span>
       </summary>
       <div
         id="details-content"
         className="gi-details-text"
-        aria-hidden={open ? 'false' : 'true'}
+        aria-hidden={isOpen ? 'false' : 'true'}
       >
         {children}
       </div>
