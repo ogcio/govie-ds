6434eedde43599b0202be41b5c66fcc4b8ae22c6
Hamza Khan <164040832+hamza14khan@users.noreply.github.com>
2025-07-08 13:30:26 +0100

    feat: add composable form field component (#635)
    Co-authored-by: Andrea Pregnolato <andrea.pregnolato@nearform.com>

---
+++ packages/react/ds/src/forms/form-field/types.ts
@@ -0,0 +1,17 @@
+import { FieldsetHTMLAttributes } from 'react';
+import { ErrorTextProps } from '../../error-text/types.js';
+import { HintTextProps } from '../../hint-text/types.js';
+import { LabelTextProps } from '../../label/types.js';
+
+export type FormFieldProps = {
+  /** @deprecated Use <FormFieldError> instead */
+  error?: ErrorTextProps;
+
+  /** @deprecated Use <FormFieldHint> instead */
+  hint?: HintTextProps;
+
+  /** @deprecated Use <FormFieldLabel> instead */
+  label?: LabelTextProps;
+
+  className?: string;
+} & Omit<FieldsetHTMLAttributes<HTMLFieldSetElement>, 'style'>;
