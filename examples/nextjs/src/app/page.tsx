064ee786fcf3b44b7a59804b99a79b779f1a6e53
hamza14khan <hamzaubit14@gmail.com>
2025-07-16 16:37:08 +0100

    fix: update select next and textarea with controlled props

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import NextLink from "next/link";
 import { ComboBoxProps, CookieBannerProps } from "@/props";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, FormProvider } from "react-hook-form";
 import {
   Alert,
   BreadcrumbCurrentLink,
@@ -48,6 +48,8 @@ import {
   PhaseBanner,
   ProgressBar,
   ProgressStepper,
+  SelectItemNext,
+  SelectNext,
   Stack,
   StepItem,
   SummaryList,
@@ -107,29 +109,354 @@ const headerProps: HeaderProps = {
 };
 
 function MyForm() {
-  const { handleSubmit, control } = useForm({
+  const methods = useForm({
     defaultValues: {
-      myText: "",
+      inputText: "",
+      textArea: "",
+      selectOption: "",
     },
   });
+
+  const { handleSubmit, control, reset } = methods;
 
   const onSubmit = (data: any) => {
     console.log("Form Data:", data);
   };
 
+  const handleClear = () => {
+    reset();
+  };
+
+  const selectOptions: string[] = [
+    "Topic 1",
+    "Topic 2",
+    "Topic 3",
+    "Topic 4",
+    "Topic 5",
+    "Topic 6",
+    "Topic 7",
+    "Topic 8",
+    "Topic 9",
+    "Topic 10",
+    "Topic 11",
+    "Topic 12",
+    "Topic 13",
+    "Topic 14",
+    "Topic 15",
+    "Topic 16",
+    "Topic 17",
+    "Topic 18",
+    "Topic 19",
+    "Topic 20",
+    "Topic 21",
+    "Topic 22",
+    "Topic 23",
+    "Topic 24",
+    "Topic 25",
+    "Topic 26",
+    "Topic 27",
+    "Topic 28",
+    "Topic 29",
+    "Topic 30",
+    "Topic 31",
+    "Topic 32",
+    "Topic 33",
+    "Topic 34",
+    "Topic 35",
+    "Topic 36",
+    "Topic 37",
+    "Topic 38",
+    "Topic 39",
+    "Topic 40",
+    "Topic 41",
+    "Topic 42",
+    "Topic 43",
+    "Topic 44",
+    "Topic 45",
+    "Topic 46",
+    "Topic 47",
+    "Topic 48",
+    "Topic 49",
+    "Topic 50",
+    "Topic 51",
+    "Topic 52",
+    "Topic 53",
+    "Topic 54",
+    "Topic 55",
+    "Topic 56",
+    "Topic 57",
+    "Topic 58",
+    "Topic 59",
+    "Topic 60",
+    "Topic 61",
+    "Topic 62",
+    "Topic 63",
+    "Topic 64",
+    "Topic 65",
+    "Topic 66",
+    "Topic 67",
+    "Topic 68",
+    "Topic 69",
+    "Topic 70",
+    "Topic 71",
+    "Topic 72",
+    "Topic 73",
+    "Topic 74",
+    "Topic 75",
+    "Topic 76",
+    "Topic 77",
+    "Topic 78",
+    "Topic 79",
+    "Topic 80",
+    "Topic 81",
+    "Topic 82",
+    "Topic 83",
+    "Topic 84",
+    "Topic 85",
+    "Topic 86",
+    "Topic 87",
+    "Topic 88",
+    "Topic 89",
+    "Topic 90",
+    "Topic 91",
+    "Topic 92",
+    "Topic 93",
+    "Topic 94",
+    "Topic 95",
+    "Topic 96",
+    "Topic 97",
+    "Topic 98",
+    "Topic 99",
+    "Topic 100",
+    "Topic 101",
+    "Topic 102",
+    "Topic 103",
+    "Topic 104",
+    "Topic 105",
+    "Topic 106",
+    "Topic 107",
+    "Topic 108",
+    "Topic 109",
+    "Topic 110",
+    "Topic 111",
+    "Topic 112",
+    "Topic 113",
+    "Topic 114",
+    "Topic 115",
+    "Topic 116",
+    "Topic 117",
+    "Topic 118",
+    "Topic 119",
+    "Topic 120",
+    "Topic 121",
+    "Topic 122",
+    "Topic 123",
+    "Topic 124",
+    "Topic 125",
+    "Topic 126",
+    "Topic 127",
+    "Topic 128",
+    "Topic 129",
+    "Topic 130",
+    "Topic 131",
+    "Topic 132",
+    "Topic 133",
+    "Topic 134",
+    "Topic 135",
+    "Topic 136",
+    "Topic 137",
+    "Topic 138",
+    "Topic 139",
+    "Topic 140",
+    "Topic 141",
+    "Topic 142",
+    "Topic 143",
+    "Topic 144",
+    "Topic 145",
+    "Topic 146",
+    "Topic 147",
+    "Topic 148",
+    "Topic 149",
+    "Topic 150",
+    "Topic 151",
+    "Topic 152",
+    "Topic 153",
+    "Topic 154",
+    "Topic 155",
+    "Topic 156",
+    "Topic 157",
+    "Topic 158",
+    "Topic 159",
+    "Topic 160",
+    "Topic 161",
+    "Topic 162",
+    "Topic 163",
+    "Topic 164",
+    "Topic 165",
+    "Topic 166",
+    "Topic 167",
+    "Topic 168",
+    "Topic 169",
+    "Topic 170",
+    "Topic 171",
+    "Topic 172",
+    "Topic 173",
+    "Topic 174",
+    "Topic 175",
+    "Topic 176",
+    "Topic 177",
+    "Topic 178",
+    "Topic 179",
+    "Topic 180",
+    "Topic 181",
+    "Topic 182",
+    "Topic 183",
+    "Topic 184",
+    "Topic 185",
+    "Topic 186",
+    "Topic 187",
+    "Topic 188",
+    "Topic 189",
+    "Topic 190",
+    "Topic 191",
+    "Topic 192",
+    "Topic 193",
+    "Topic 194",
+    "Topic 195",
+    "Topic 196",
+    "Topic 197",
+    "Topic 198",
+    "Topic 199",
+    "Topic 200",
+  ];
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="myText"
-        control={control}
-        rules={{ maxLength: 50 }}
-        render={({ field, fieldState }) => (
-          <>
-            <TextArea id="textarea-id" maxChars={50} {...field} />
-          </>
-        )}
-      />
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container
+          className="
+            p-0
+            w-full
+            border
+            border-[--gieds-color-gray-200]
+            bg-white
+            rounded-lg
+            shadow-lg
+          "
+          id="card-container"
+        >
+          <Container className="px-4 pt-4 pb-0 md:px-8 md:pt-8 md:pb-6">
+            <Container className="p-0 pb-2">
+              <Heading as="h3" id="heading">
+                My Form
+              </Heading>
+            </Container>
+
+            <Container className="p-0 pb-8">
+              <Heading
+                as="h5"
+                className="font-normal text-[--gieds-color-gray-600]"
+                id="subheading"
+              >
+                Please fill in the fields
+              </Heading>
+            </Container>
+
+            <Container className="flex flex-col items-center p-0 gap-4 w-full lg:w-[480px] mx-auto">
+              <FormField
+                className="
+                  w-full
+                  font-[family-name:var(--gieds-font-family-primary)]
+                "
+                label={{ text: "Input Text" }}
+              >
+                <Controller
+                  control={control}
+                  name="inputText"
+                  render={({ field }) => (
+                    <InputText
+                      {...field}
+                      data-testid="input-text-id"
+                      id="input-text-id"
+                      className="w-full"
+                    />
+                  )}
+                />
+              </FormField>
+              <FormField
+                className="
+                  w-full
+                  font-[family-name:var(--gieds-font-family-primary)]
+                "
+                label={{ text: "Text Area" }}
+              >
+                <Controller
+                  control={control}
+                  name="textArea"
+                  render={({ field }) => (
+                    <TextArea
+                      {...field}
+                      cols={100}
+                      rows={4}
+                      id="textarea-id-0"
+                      data-testid="textarea-id-0"
+                      className="w-full"
+                      maxChars={100}
+                    />
+                  )}
+                />
+              </FormField>
+              <FormField
+                className="
+                  w-full
+                  font-[family-name:var(--gieds-font-family-primary)]
+                "
+                label={{ text: "Select an Option" }}
+              >
+                <Controller
+                  control={control}
+                  name="selectOption"
+                  render={({ field }) => (
+                    <SelectNext
+                      {...field}
+                      enableSearch
+                      id="select-option-id"
+                      data-testid="select-option-id"
+                      className="w-full"
+                      onChange={(value: any) => field.onChange(value)}
+                    >
+                      <SelectItemNext value="">Please select</SelectItemNext>
+                      {selectOptions.map((opt) => (
+                        <SelectItemNext key={opt} value={opt}>
+                          {opt}
+                        </SelectItemNext>
+                      ))}
+                    </SelectNext>
+                  )}
+                />
+              </FormField>
+              <Container className="flex gap-2">
+                <Button
+                  type="submit"
+                  data-testid="submit-button"
+                  variant="primary"
+                >
+                  Submit
+                </Button>
+                <Button
+                  type="button"
+                  data-testid="clear-button"
+                  variant="secondary"
+                  onClick={handleClear}
+                >
+                  Clear
+                </Button>
+              </Container>
+            </Container>
+          </Container>
+        </Container>
+      </form>
+    </FormProvider>
   );
 }
 
@@ -326,7 +653,7 @@ export default function Home() {
           </FormField>
           <FormField>
             <FormFieldLabel htmlFor="textarea-id">
-              Textarea with React Hook Form
+              Inputs with React Hook Form
             </FormFieldLabel>
             <FormFieldHint>Hint: This is a helpful hint.</FormFieldHint>
             <MyForm />
@@ -338,7 +665,6 @@ export default function Home() {
             <FormFieldHint>Hint: This is a helpful hint.</FormFieldHint>
             <TextArea id="textarea-id2" maxChars={50} />
           </FormField>
-
           <span className="material-symbols-outlined">face</span>
           <div>
             <Modal triggerButton={<Button>Open Modal</Button>}>
