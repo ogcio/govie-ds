d2454b40fff9098ab0651306e374b1c16349d49f
hamza14khan <hamzaubit14@gmail.com>
2025-07-23 09:32:59 +0100

    feat: add table pagination to tanstack

---
+++ packages/react/ds/src/table/pagination.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { IconButton } from '../icon-button/icon-button.js';
+import { translate as t } from '../i18n/utility.js';
+
+export type TablePaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+};
+
+export const TablePagination: React.FC<TablePaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="gi-table-pagination">
+      <IconButton
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(1)}
+        appearance="dark"
+        variant="flat"
+        icon={{
+          // icon: 'first_page',
+          icon: 'chevron_left',
+        }}
+      />
+      <IconButton
+        disabled={currentPage === 1}
+        onClick={handlePrevious}
+        appearance="dark"
+        variant="flat"
+        icon={{
+          icon: 'chevron_left',
+        }}
+      />
+      <span className="gi-text-md" aria-live="polite">
+        <span className="gi-font-bold">
+          {t('table.pagination.page', {
+            currentPage,
+            defaultValue: `Page ${currentPage} `,
+          })}
+        </span>
+        {t('table.pagination.ofTotal', {
+          totalPages,
+          defaultValue: `of ${totalPages}`,
+        })}
+      </span>
+      <IconButton
+        onClick={handleNext}
+        disabled={currentPage === totalPages}
+        appearance="dark"
+        variant="flat"
+        icon={{
+          icon: 'chevron_right',
+        }}
+      />
+      <IconButton
+        disabled={currentPage === totalPages}
+        onClick={() => onPageChange(totalPages)}
+        appearance="dark"
+        variant="flat"
+        icon={{
+          // icon: 'last_page',
+          icon: 'chevron_right',
+        }}
+      />
+    </div>
+  );
+};
+
+export default TablePagination;
