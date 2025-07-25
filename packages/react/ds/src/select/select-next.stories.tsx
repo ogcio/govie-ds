0cd0a31e21ec2ca0c29fe8768c18ea7899644e10
Ramon Mulia <ramonmulia@gmail.com>
2025-07-25 05:41:39 -0300

    fixing menu height

--- packages/react/ds/src/select/select-next.stories.tsx
+++ packages/react/ds/src/select/select-next.stories.tsx
@@ -359,3 +359,63 @@ export const Controlled: StoryObj = {
     });
   },
 };
+
+export const WithLongList: StoryObj = {
+  render: () => {
+    const topics: string[] = [
+      'Topic 1',
+      'Topic 2',
+      'Topic 3',
+      'Topic 4',
+      'Topic 5',
+      'Topic 6',
+      'Topic 7',
+      'Topic 8',
+      'Topic 9',
+      'Topic 10',
+      'Topic 11',
+      'Topic 12',
+      'Topic 13',
+      'Topic 14',
+      'Topic 15',
+      'Topic 16',
+      'Topic 17',
+      'Topic 18',
+      'Topic 19',
+      'Topic 20',
+      'Topic 21',
+      'Topic 22',
+      'Topic 23',
+      'Topic 24',
+      'Topic 25',
+      'Topic 26',
+      'Topic 27',
+      'Topic 28',
+      'Topic 29',
+      'Topic 30',
+      'Topic 31',
+      'Topic 32',
+      'Topic 33',
+      'Topic 34',
+      'Topic 35',
+      'Topic 36',
+      'Topic 37',
+      'Topic 38',
+      'Topic 39',
+      'Topic 40',
+    ];
+
+    return (
+      <FormField className="gi-w-56">
+        <FormFieldLabel>Long List Select</FormFieldLabel>
+        <SelectNext aria-label="Select" id="select-controlled">
+          {topics.map((topic) => (
+            <SelectItemNext key={topic} value={topic}>
+              {topic}
+            </SelectItemNext>
+          ))}
+        </SelectNext>
+      </FormField>
+    );
+  },
+};
