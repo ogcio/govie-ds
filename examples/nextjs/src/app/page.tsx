e88c9c6aacf7b19e1a9d13b327ad50d2134b90ad
hamza14khan <hamzaubit14@gmail.com>
2025-08-20 15:12:18 +0100

    chore: update escaped values

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1244,9 +1244,9 @@ export default function Home() {
                     </h5>
                     <Details label="Help with Nationality">
                       We need to know your nationality so we can work out which
-                      elections you're entitled to vote in. If you cannot
-                      provide your nationality, you'll have to send copies of
-                      identity documents through the post.
+                      elections you&apos;re entitled to vote in. If you cannot
+                      provide your nationality, you&apos;ll have to send copies
+                      of identity documents through the post.
                     </Details>
                   </div>
                 </div>
