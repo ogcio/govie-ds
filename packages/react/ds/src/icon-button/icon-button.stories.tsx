644d11c0f9449a5217c080b5c07126fa5fc1c606
Ramon Mulia <ramonmulia@gmail.com>
2025-08-21 13:06:43 -0300

    fix: lint

--- packages/react/ds/src/icon-button/icon-button.stories.tsx
+++ packages/react/ds/src/icon-button/icon-button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn, expect  } from 'storybook/test';
+import { fn, expect } from 'storybook/test';
 import { IconButton } from './icon-button.js';
 
 const meta = {
