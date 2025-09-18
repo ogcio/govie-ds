d334f5ea98570d5ab49b3f559c9d886d6881cdea
hamza14khan <hamzaubit14@gmail.com>
2025-09-18 12:48:41 +0100

    fix: add data table column size for header and cell

--- packages/react/ds/src/table/table-head.tsx
+++ packages/react/ds/src/table/table-head.tsx
@@ -1,8 +1,29 @@
+import clsx from 'clsx';
 import React from 'react';
+
+export type TableHeadSize =
+  | 'xs-fixed'
+  | 'sm-fixed'
+  | 'md-fixed'
+  | 'lg-flex'
+  | 'fluid';
+
+export interface TableHeadProps
+  extends React.TableHTMLAttributes<HTMLTableSectionElement> {
+  size?: TableHeadSize;
+}
 
 export function TableHead({
   children,
+  size,
+  className,
   ...props
-}: React.TableHTMLAttributes<HTMLTableSectionElement>) {
-  return <thead {...props}>{children}</thead>;
+}: TableHeadProps) {
+  const sizeClass = size ? `gi-table-th-${size}` : undefined;
+
+  return (
+    <thead {...props} className={clsx(sizeClass, className)}>
+      {children}
+    </thead>
+  );
 }
