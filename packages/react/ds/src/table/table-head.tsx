a933f4a314e3ca2b40ef89c365abf1640d6be9d9
hamza14khan <hamzaubit14@gmail.com>
2025-09-18 13:16:04 +0100

    fix: revert table head changes and apply to table header

--- packages/react/ds/src/table/table-head.tsx
+++ packages/react/ds/src/table/table-head.tsx
@@ -1,29 +1,8 @@
-import clsx from 'clsx';
 import React from 'react';
-
-export type TableHeadSize =
-  | 'xs-fixed'
-  | 'sm-fixed'
-  | 'md-fixed'
-  | 'lg-flex'
-  | 'fluid';
-
-export interface TableHeadProps
-  extends React.TableHTMLAttributes<HTMLTableSectionElement> {
-  size?: TableHeadSize;
-}
 
 export function TableHead({
   children,
-  size,
-  className,
   ...props
-}: TableHeadProps) {
-  const sizeClass = size ? `gi-table-th-${size}` : undefined;
-
-  return (
-    <thead {...props} className={clsx(sizeClass, className)}>
-      {children}
-    </thead>
-  );
+}: React.TableHTMLAttributes<HTMLTableSectionElement>) {
+  return <thead {...props}>{children}</thead>;
 }
