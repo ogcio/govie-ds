f612c7996d5b0d40d545b628d0dae15328bda075
hamza14khan <hamzaubit14@gmail.com>
2025-10-31 18:48:35 +0000

    fix: revert input text stories

--- packages/react/ds/src/input-text/input-text.stories.tsx
+++ packages/react/ds/src/input-text/input-text.stories.tsx
@@ -259,7 +259,7 @@ export const InputLength: Story = {
 };
 
 export const DateInput: Story = {
-  args: { id: 'input-text-id', type: 'date', value: '2023-01-01' },
+  args: { id: 'input-text-id', type: 'date' },
   render: (props) => (
     <FormField>
       <FormFieldLabel htmlFor="input-text-id">Date input</FormFieldLabel>
