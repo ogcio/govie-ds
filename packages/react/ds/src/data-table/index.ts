30f33d0ef808ad4c2708146b33912cc916985321
Dmitry Badylevich <dmitry.badylevich@nearform.com>
2025-11-24 10:50:29 +0100

    feat(AB#32875): export all DataTable components for the public usage (#779)

---
+++ packages/react/ds/src/data-table/index.ts
@@ -0,0 +1,27 @@
+export type {
+  DataTableFooterProps,
+  DataTableFooterTypeProps,
+} from './data-table-footer.js';
+export {
+  DataTableFooter,
+  DataTableFooterStart,
+  DataTableFooterCenter,
+  DataTableFooterEnd,
+} from './data-table-footer.js';
+
+export {
+  DataTableHeader,
+  DataTableHeaderSearch,
+  DataTableHeaderActions,
+  DataTableHeaderFilter,
+  DataTableHeaderFilterList,
+  DataTableHeaderFilterContent,
+  DataTableHeaderFilterContentTitle,
+  DataTableHeaderFilterActions,
+} from './data-table-header.js';
+
+export { DataTableSelectedRowsBanner } from './data-table-selected-rows.js';
+export type { DataTableSelectedRowsBannerProps } from './data-table-selected-rows.js';
+
+export { EditableTableCell } from './editable-table-cell.js';
+export type { EditorTableCellConfig, EditorTableCellProps } from './types.js';
