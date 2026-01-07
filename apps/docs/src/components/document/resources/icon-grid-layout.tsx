968da9c2173dcaf6fd1582511289689e2c8c916c
Hamza Khan <164040832+hamza14khan@users.noreply.github.com>
2026-01-07 10:15:58 +0000

    feat(AB#33374): Add a new button to download SVG icons (#811)
    Co-authored-by: DmitryB <dmitry.badylevich@nearform.com>

--- apps/docs/src/components/document/resources/icon-grid-layout.tsx
+++ apps/docs/src/components/document/resources/icon-grid-layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Icon, Icons, Paragraph } from '@ogcio/design-system-react';
+import { DownloadIconButton } from './download-icon-button';
 import { CopyToClipboardButton } from './copy-to-clipboard-button';
 import { titleCase } from '@/lib/utilities';
 
@@ -10,14 +11,21 @@ export function IconGridLayout() {
     <ul className="flex flex-wrap gap-2 p-0 gi-not-prose">
       {icons.map((icon) => {
         const iconName = titleCase(icon).replaceAll(' Alt', '');
+        const safeName = iconName.toLowerCase().trim().replace(/\s+/g, '_');
         return (
-          <li key={icon} className="inline-block w-32 h-32">
-            <div className="border rounded-md hover:bg-gray-50 transition-colors relative">
-              <div className="flex justify-center p-6">
+          <li key={icon} className="inline-block w-32">
+            <div className="border rounded-md transition-colors relative">
+              <div className="flex justify-center px-10 py-12">
                 <Icon icon={icon} size="lg" />
               </div>
-              <div className="absolute bottom-1 right-2">
+              <div className="absolute bottom-1 right-1">
                 <CopyToClipboardButton text={icon} />
+              </div>
+              <div className="absolute bottom-1 right-10">
+                <DownloadIconButton
+                  name={icon}
+                  href={`/icons/${safeName}.svg`}
+                />
               </div>
             </div>
             <Paragraph size="sm" className="text-center">
