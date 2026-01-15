c2d1000c30b1e26b66ace64975ddba6e316b3bec
Ramon Mulia <ramonmulia@gmail.com>
2026-01-15 11:22:02 -0300

    chore(AB#33670): generate Angular wrappers from Stencil web components

---
+++ examples/angular/src/server.ts
@@ -0,0 +1,54 @@
+// src/server.ts
+import express from 'express';
+import { join } from 'node:path';
+import { existsSync } from 'node:fs';
+
+import { CommonEngine } from '@angular/ssr/node';
+import bootstrap from './main.server';
+
+import { renderToString } from '@ogcio/govie-component-library/hydrate';
+
+export function app(): express.Express {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/ogcio-demo/browser'); // <-- adjust app name
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
+
+  const commonEngine = new CommonEngine();
+
+  server.use(express.static(distFolder, { maxAge: '1y', index: false }));
+
+  server.get('*', async (req, res, next) => {
+    try {
+      const html = await commonEngine.render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: req.originalUrl,
+        publicPath: distFolder,
+        providers: [{ provide: 'REQUEST', useValue: req }],
+      });
+
+      //full SSR
+      const hydrated = await renderToString(html, {
+        prettyHtml: true,
+      });
+
+      res.status(200).send(hydrated.html);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  return server;
+}
+
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+  const server = app();
+  server.listen(port, () => {
+    console.log(`SSR server listening on http://localhost:${port}`);
+  });
+}
+
+run();
--- packages/govie-component-library/ds/stencil.config.ts
+++ packages/govie-component-library/ds/stencil.config.ts
@@ -3,27 +3,29 @@ import { postcss } from '@stencil-community/postcss';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import { reactOutputTarget } from '@stencil/react-output-target';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'govie-component-library',
-
-  enableCache: true,
   sourceMap: true,
-
   globalStyle: 'styles.css',
-
   plugins: [
     postcss({
       plugins: [tailwindcss, autoprefixer],
     }),
   ],
-  //buildEs5: false,
-
   outputTargets: [
     reactOutputTarget({
       outDir: '../../govie-react/ds/src/components',
       hydrateModule: '@ogcio/govie-component-library/hydrate',
       clientModule: '@ogcio/govie-react',
+    }),
+    angularOutputTarget({
+      componentCorePackage: '@ogcio/govie-component-library',
+      directivesProxyFile:
+        '../../govie-angular/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile:
+        '../../govie-angular/projects/component-library/src/lib/stencil-generated/index.ts',
     }),
     {
       type: 'dist-hydrate-script',
@@ -37,4 +39,7 @@ export const config: Config = {
     },
     { type: 'www', serviceWorker: null },
   ],
+  extras: {
+    enableImportInjection: true,
+  },
 };
