233efd09b4803abe72fc578165804721c5142cda
Ramon Mulia <ramonmulia@gmail.com>
2026-01-16 16:59:20 -0300

    feat(AB#33678): example of angular

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { defineCustomElements } from '@ogcio/govie-component-library/loader';
+import {
+  GovieButton,
+  GovieParagraph,
+} from '@ogcio/govie-angular/dist/component-library';
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, GovieButton, GovieParagraph],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = "ogcio-demo";
+export class AppComponent implements OnInit {
+  title = 'ogcio-demo';
+
+  ngOnInit(): void {
+    defineCustomElements();
+  }
 }
