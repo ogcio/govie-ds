185a18ff0a067c9601823baf6373a0c580a32109
Ramon Mulia <ramonmulia@gmail.com>
2026-01-21 10:08:23 -0300

    chore(AB#34250): removing unecessary bundles, styles and scripts

--- examples/angular/src/app/app.component.spec.ts
+++ examples/angular/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -17,15 +17,15 @@ describe("AppComponent", () => {
   it(`should have the 'ogcio-demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("ogcio-demo");
+    expect(app.title).toEqual('ogcio-demo');
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, ogcio-demo"
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Design System Showcase',
     );
   });
 });
