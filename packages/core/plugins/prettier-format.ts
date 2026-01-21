ff1656102af9ef14376ce5b757a93c50f99bb892
DmitryB <dmitry.badylevich@nearform.com>
2026-01-21 10:41:04 +0100

    feat(AB#33895): fix annotations extension
    Update generated file.
    Fix typings for the prettier plugin.

--- packages/core/plugins/prettier-format.ts
+++ packages/core/plugins/prettier-format.ts
@@ -1,6 +1,7 @@
 import { MitosisPlugin } from '@builder.io/mitosis'
 import format from '@prettier/sync'
 import config from '@ogcio/design-system-prettier-config'
+import type { Options } from 'prettier'
 
 /**
  * Custom Prettier formatter for Mitosis code generation.
@@ -10,7 +11,7 @@ const prettierFormat: MitosisPlugin = () => ({
     post: (code) =>
       format.format(code, {
         parser: 'typescript',
-        ...(config as any),
+        ...(config as Options),
       }),
   },
 })
