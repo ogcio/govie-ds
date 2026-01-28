42a83a1cedc4249fe6012d8a8830f3e72e8f6d33
Ramon Mulia <ramonmulia@gmail.com>
2026-01-28 10:17:44 -0300

    chore(AB#34250): adding govie theme + tailwind configuration

--- examples/angular/tailwind.config.ts
+++ examples/angular/tailwind.config.ts
@@ -1,7 +1,28 @@
-import { Config } from 'tailwindcss';
+import { createTheme } from '@ogcio/design-system-tailwind';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  prefix: 'gi-',
   content: ['./src/**/*.{html,ts}'],
+  theme: createTheme(),
+  plugins: [typography],
+  safelist: [
+    'gi-prose',
+    'gi-not-prose',
+    'gi-max-w-none',
+    'gi-grid',
+    'gi-stroke-gray-950',
+    'gi-h-screen',
+    'gi-w-full',
+    { pattern: /gi-align-.*/ },
+    { pattern: /gi-block/, variants: ['sm', 'md', 'lg', 'xl', '2xl'] },
+    { pattern: /gi-hidden/, variants: ['sm', 'md', 'lg', 'xl', '2xl'] },
+    { pattern: /gi-grid-.*/ },
+    { pattern: /gi-col-.*/ },
+    { pattern: /gi-row-.*/ },
+    { pattern: /gi-gap-.*/, variants: ['sm', 'md', 'lg', 'xl', '2xl'] },
+  ],
 };
 
 export default config;
