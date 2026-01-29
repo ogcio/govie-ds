ee8157052f1d2bc9bc120ef7a48c490b60ee42d5
Ramon Mulia <ramonmulia@gmail.com>
2026-01-29 04:35:01 -0300

    feat(AB#34250): Removing unnecessary bundles, styles and scripts in Angular example (#826)

--- examples/angular/src/main.ts
+++ examples/angular/src/main.ts
@@ -1,9 +1,8 @@
+/* eslint-disable unicorn/prefer-top-level-await */
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-try {
-  await bootstrapApplication(AppComponent, appConfig);
-} catch (error) {
-  console.error(error);
-}
+bootstrapApplication(AppComponent, appConfig).catch((error) =>
+  console.error(error),
+);
