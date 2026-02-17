1a575081343c2e1f1d6a0b2350b15fde31176b64
Ramon Mulia <ramonmulia@gmail.com>
2026-02-17 16:46:27 -0300

    feat(AB#35163): render legend instead of label when htmlFor is omitted

--- packages/react/ds/src/forms/form-field/types.ts
+++ packages/react/ds/src/forms/form-field/types.ts
@@ -1,11 +1,22 @@
-import { FieldsetHTMLAttributes } from 'react';
+import { FieldsetHTMLAttributes, PropsWithChildren } from 'react';
 import { ErrorTextProps } from '../../error-text/types.js';
 import { HintTextProps } from '../../hint-text/types.js';
-import { LabelTextProps } from '../../label/types.js';
+import { LabelSizeType, LabelTextProps } from '../../label/types.js';
 
-export type FormFiledLabelProps = LabelTextProps & {
+export type LegendProps = PropsWithChildren<
+  React.HTMLAttributes<HTMLLegendElement> & {
+    text?: string | React.ReactElement;
+    size?: LabelSizeType;
+    secondaryLabel?: React.ReactNode;
+    htmlFor?: never;
+  }
+>;
+
+export type LabelProps = LabelTextProps & {
   secondaryLabel?: React.ReactNode;
 };
+
+export type FormFieldLabelProps = LabelProps | LegendProps;
 
 export type FormFieldProps = {
   /** @deprecated Use <FormFieldError> instead */
