988e2b3b94d705fadab30ca1c08586a5365fc31c
Ademsk1 <adam.illoul24@gmail.com>
2026-03-04 14:45:06 +0000

    feat(AB#35132): footer multiline

--- packages/react/ds/src/footer/footer.tsx
+++ packages/react/ds/src/footer/footer.tsx
@@ -103,7 +103,10 @@ export function Footer({
               defaultValue: 'Footer Secondary Slot',
             })}
           >
-            <div className="gi-w-fit gi-mt-8 md:gi-mt-0 lg:gi-ml-auto lg:gi-flex-none gi-rounded-sm gi-focus-state-outline gi-focus-within-state-outline gi-focus-visible-state-outline">
+            <div
+              className="gi-w-fit gi-mt-8 md:gi-mt-0 lg:gi-ml-autolg:gi-flex-none gi-rounded-sm 
+                         gi-focus-state-outline gi-focus-within-state-outline gi-focus-visible-state-outline"
+            >
               {renderLogo()}
             </div>
             {secondarySlot && (
