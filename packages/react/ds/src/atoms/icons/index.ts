110b2e85459f4eec123e95d88e83282f68a84254
Ademsk1 <adam.illoul24@gmail.com>
2026-03-11 15:00:11 +0000

    feat(AB#35938): adds in mitosis icons to be used

--- packages/react/ds/src/atoms/icons/index.ts
+++ packages/react/ds/src/atoms/icons/index.ts
@@ -1,6 +1,10 @@
-export { default as KeyboardArrowDown } from './KeyboardArrowDown';
-export { default as KeyboardArrowUp } from './KeyboardArrowUp';
-export { default as Close } from './Close';
-export { default as Visibility } from './Visibility';
-export { default as VisibilityOff } from './VisibilityOff';
-export * from './types'
+export { default as KeyboardArrowDown } from "./KeyboardArrowDown";
+export { default as KeyboardArrowUp } from "./KeyboardArrowUp";
+export { default as Close } from "./Close";
+export { default as Visibility } from "./Visibility";
+export { default as VisibilityOff } from "./VisibilityOff";
+export { default as Warning } from "./Warning";
+export { default as CheckCircle } from "./CheckCircle";
+export { default as Error } from "./Error";
+export { default as Info } from "./Info";
+export * from "./types"
