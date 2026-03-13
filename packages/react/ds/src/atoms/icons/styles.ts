974fcd099d6fcc9fb79f9a913faa9e9db1792668
Ademsk1 <adam.illoul24@gmail.com>
2026-03-13 16:22:04 +0000

    feat(AB#35938): remove redundant style for react/angular

--- packages/react/ds/src/atoms/icons/styles.ts
+++ packages/react/ds/src/atoms/icons/styles.ts
@@ -3,7 +3,7 @@ export const iconStyles = tv({
   base: 'gi-shrink-0 gi-block',
   variants: {
     inline: {
-      true: 'gi-inline-block',
+      true: 'gi-inline-block'
     },
     disabled: {
       true: 'gi-fill-gray-700'
