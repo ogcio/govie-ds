5146d405619d8b5f538112928c3322c71b180206
DmitryB <dmitry.badylevich@nearform.com>
2026-03-13 14:13:40 +0100

    chore(AB#36197): set fixed version of Docs dependencies
    Simplify `copy-html-ds.js` script.
    Update `pnpm-lock.yaml` after fixing dependencies.

--- apps/docs/scripts/copy-html-ds.js
+++ apps/docs/scripts/copy-html-ds.js
@@ -5,20 +5,18 @@ import { fileURLToPath } from 'node:url';
 
 const require = createRequire(import.meta.url);
 
-(function copyHtmlToTemplate() {
-  const docsDir = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
-  const destDir = path.join(docsDir, 'public/templates');
+const docsDir = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+const destDir = path.join(docsDir, 'public/templates');
 
-  fs.copyFileSync(
-    require.resolve('@ogcio/design-system-html'),
-    path.join(destDir, 'govie-frontend.umd.js'),
-  );
-  fs.copyFileSync(
-    require.resolve('@ogcio/design-system-html/styles.css'),
-    path.join(destDir, 'styles.css'),
-  );
-  fs.copyFileSync(
-    require.resolve('@ogcio/theme-govie/theme.css'),
-    path.join(destDir, 'theme.css'),
-  );
-})();
+fs.copyFileSync(
+  require.resolve('@ogcio/design-system-html'),
+  path.join(destDir, 'govie-frontend.umd.js'),
+);
+fs.copyFileSync(
+  require.resolve('@ogcio/design-system-html/styles.css'),
+  path.join(destDir, 'styles.css'),
+);
+fs.copyFileSync(
+  require.resolve('@ogcio/theme-govie/theme.css'),
+  path.join(destDir, 'theme.css'),
+);
