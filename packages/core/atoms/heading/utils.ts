567d73f1d1cbaf3d8450bb58dad5dbf60b41b3a1
Ramon Mulia <ramonmulia@gmail.com>
2026-03-23 09:17:48 -0300

    feat(AB#35883): fixes

--- packages/core/atoms/heading/utils.ts
+++ packages/core/atoms/heading/utils.ts
@@ -1,4 +1,4 @@
-import { Props, Size } from './types';
+import { type Props, Size } from './types';
 
 export const getSize = (size: Props['size'], defaultSize: Props['size']): Props['size'] =>
   size && Object.values(Size).includes(size) ? size : defaultSize;
