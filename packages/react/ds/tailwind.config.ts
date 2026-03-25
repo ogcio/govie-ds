940f2b167f6cf2021577241f25c7a0d6a8106e76
Ademsk1 <adam.illoul24@gmail.com>
2026-03-25 17:30:21 +0000

    fix(AB#36244): use in typography.css, apply :where for overruling

--- packages/react/ds/tailwind.config.ts
+++ packages/react/ds/tailwind.config.ts
@@ -1,29 +1,12 @@
 import { createTheme } from '@ogcio/design-system-tailwind';
 import typography from '@tailwindcss/typography';
 import { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   prefix: 'gi-',
   content: ['./src/**/*.{ts,tsx}'],
   theme: createTheme(),
-  plugins: [
-    typography,
-    plugin(({ addUtilities }) => {
-      addUtilities({
-        '.underline': {
-          // whenever text is underlined without offset specified, use 0.2em
-          textUnderlineOffset: '0.2em',
-        },
-        '@supports (-moz-appearance: none)': {
-          '.underline': {
-            // Firefox skips the underline for descending characters at 0.2em for the given font Lato, so we increase it to 0.23em
-            textUnderlineOffset: '0.23em',
-          },
-        },
-      });
-    }),
-  ],
+  plugins: [typography],
   safelist: [
     'gi-prose',
     'gi-not-prose',
