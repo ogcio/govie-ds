fe3eefed7b2dbe0eba63a9fd398992d33dcc4caf
Ramon Mulia <ramonmulia@gmail.com>
2026-03-25 17:35:30 -0300

    chore(AB#36252): fixing packages

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@storybook/addon-docs',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-docs', '@chromatic-com/storybook'],
   framework: {
     name: '@storybook/nextjs-vite',
     options: {},
