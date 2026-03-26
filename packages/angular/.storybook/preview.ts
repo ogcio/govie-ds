a7779ea41568221da44cf6551aae4e303713fe10
Ramon Mulia <ramonmulia@gmail.com>
2026-03-26 15:35:06 -0300

    chore(AB#36252): adding angular storybook config

--- packages/angular/.storybook/preview.ts
+++ packages/angular/.storybook/preview.ts
@@ -1,7 +1,13 @@
 import type { Preview } from '@storybook/angular';
+import DocumentationTemplate from './DocumentationTemplate.mdx';
 
 const preview: Preview = {
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      page: DocumentationTemplate,
+    },
+  },
 };
 
 export default preview;
