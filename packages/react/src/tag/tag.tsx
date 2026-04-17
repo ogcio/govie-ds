8b52836ebcfe050e1a9c7fe8cff7d48b6361f90d
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2026-04-17 09:14:00 +0100

    chore(AB#36572): react dependencies audit (#922)
    Co-authored-by: Dmitry Badylevich <dmitry.badylevich@nearform.com>

--- packages/react/src/tag/tag.tsx
+++ packages/react/src/tag/tag.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
 export const TagTypeEnum = {
   Default: 'default',
