03327e5d1303d2e3e915dcdfd9806b5430344a58
Ademsk1 <adam.illoul24@gmail.com>
2026-04-27 10:41:02 +0100

    feat(AB#35133): reset input-checkbox

--- packages/react/src/input-checkbox/input-checkbox.tsx
+++ packages/react/src/input-checkbox/input-checkbox.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useId, forwardRef } from 'react';
-import { tv } from 'tailwind-variants';
 import { cn } from '../cn.js';
 import { HintText } from '../hint-text/hint-text.js';
 import { Label } from '../label/label.js';
@@ -12,6 +11,17 @@ import {
   type InputCheckboxProps,
   type InputCheckboxTableCellProps,
 } from './types.js';
+
+const getSizeClass = (size: InputCheckboxSizeEnumType): string => {
+  switch (size) {
+    case InputCheckboxSizeEnum.Small: {
+      return 'gi-input-checkbox-small';
+    }
+    default: {
+      return 'gi-input-checkbox-medium';
+    }
+  }
+};
 
 export const getCheckboxWidth = (size?: InputCheckboxSizeEnumType) => {
   if (size === InputCheckboxSizeEnum.Small) {
@@ -39,25 +49,21 @@ export const InputCheckbox = forwardRef<HTMLInputElement, InputCheckboxProps>(
     const hasRichContent = !!children;
     const labelContent = children ?? label;
 
-    const {
-      container,
-      checkbox,
-      hintBefore,
-      label: label_,
-    } = styles({
-      hasRichContent,
-      indeterminate,
-      size,
-    });
-
     return (
       <>
-        <div className={container()} {...containerProps}>
+        <div
+          className={cn('gi-input-checkbox-container', {
+            '!gi-items-start': hasRichContent,
+          })}
+          {...containerProps}
+        >
           <Input
             type="checkbox"
             ref={ref}
             id={CheckboxId}
-            className={checkbox()}
+            className={cn(getSizeClass(size), {
+              'gi-checkbox-indeterminate': indeterminate,
+            })}
             aria-labelledby={labelContent ? `${CheckboxId}-label` : undefined}
             {...props}
           />
@@ -67,7 +73,7 @@ export const InputCheckbox = forwardRef<HTMLInputElement, InputCheckboxProps>(
               id={`${CheckboxId}-label`}
               htmlFor={hasRichContent ? undefined : CheckboxId}
               size={size}
-              className={label_()}
+              className={hasRichContent ? undefined : 'gi-cursor-pointer'}
             >
               {labelContent}
             </Label>
@@ -75,9 +81,9 @@ export const InputCheckbox = forwardRef<HTMLInputElement, InputCheckboxProps>(
         </div>
 
         {hint && (
-          <div className="gi-flex gi-gap-3">
-            <div className="gi-h-auto">
-              <div className={hintBefore()} />
+          <div className="gi-input-checkbox-hint-container">
+            <div>
+              <div className={getCheckboxWidth(size)} />
             </div>
             <HintText id={`${CheckboxId}-hint`} text={hint} />
           </div>
@@ -100,114 +106,6 @@ export const InputCheckboxTableCell: React.FC<InputCheckboxTableCellProps> = ({
     }}
   />
 );
-
-const styles = tv({
-  slots: {
-    container: 'gi-gap-3 gi-flex gi-items-center',
-    label: '',
-    hintBefore: '',
-    checkbox: [
-      // base state
-      'gi-rounded-sm',
-      'gi-focus-state-outline',
-      'gi-cursor-pointer',
-      'gi-appearance-none',
-      'gi-border-sm',
-      'gi-border-solid',
-      'gi-border-color-border-system-neutral-default',
-      // hover state
-      'hover:gi-outline',
-      'hover:gi-outline-sm',
-      'hover:gi-outline-gray-200',
-      'hover:gi-outline-offset-0',
-      // focus state
-      'focus:gi-shadow-[inset_0_0_0_1px]',
-      'focus:gi-shadow-color-border-system-neutral-interactive-hover',
-      // disabled state
-      'gi-input-disabled-state', // TODO: convert this one
-      'disabled:hover:gi-outline-none',
-      'disabled:before:gi-border-color-border-system-neutral-interactive-disabled-surface',
-      'disabled:gi-border-color-border-system-neutral-interactive-disabled-surface',
-      // checked state
-      'checked:before:gi-block',
-      'checked:before:gi--rotate-45',
-      'checked:before:gi-relative',
-      'checked:before:gi-border-l-lg',
-      'checked:before:gi-border-b-lg',
-      'checked:before:gi-border-color-border-system-neutral-default',
-    ],
-  },
-  variants: {
-    hasRichContent: {
-      true: {
-        container: 'gi-items-start',
-      },
-      false: {
-        label: 'gi-cursor-pointer',
-      },
-    },
-    indeterminate: {
-      true: {
-        checkbox: [
-          'checked:before:gi-translate-y-0',
-          'checked:before:gi-rotate-0',
-        ],
-      },
-    },
-    size: {
-      [InputCheckboxSizeEnum.Small as string]: {
-        hintBefore: 'gi-w-6',
-        checkbox: [
-          'gi-flex-none',
-          'gi-w-6',
-          'gi-h-6',
-          'checked:before:gi-w-3',
-          'checked:before:gi-h-[1px]',
-          'checked:before:gi-left-1',
-          'checked:before:gi-top-[9px]',
-          'checked:before:gi-border-b-sm',
-        ],
-      },
-      [InputCheckboxSizeEnum.Medium as string]: {
-        hintBefore: 'gi-w-8',
-        checkbox: [
-          'gi-flex-none',
-          'gi-w-8',
-          'gi-h-8',
-          'checked:before:gi-w-5',
-          'checked:before:gi-h-[1px]',
-          'checked:before:gi-left-1.5',
-          'checked:before:gi-top-[13px]',
-          'checked:before:gi-border-b-md',
-        ],
-      },
-    },
-  },
-  compoundVariants: [
-    {
-      indeterminate: true,
-      size: InputCheckboxSizeEnum.Small,
-      checkbox: [
-        'checked:before:gi-w-3',
-        'checked:before:gi-h-[1px]',
-        'checked:before:gi-left-1',
-        'checked:before:gi-top-[9px]',
-        'checked:before:gi-border-b-sm',
-      ],
-    },
-    {
-      indeterminate: true,
-      size: InputCheckboxSizeEnum.Medium,
-      checkbox: [
-        'checked:before:gi-w-4',
-        'checked:before:gi-h-[1px]',
-        'checked:before:gi-left-1.5',
-        'checked:before:gi-top-[13px]',
-        'checked:before:gi-border-b-md',
-      ],
-    },
-  ],
-});
 
 InputCheckbox.displayName = 'InputCheckbox';
 InputCheckboxTableCell.displayName = 'InputCheckboxTableCell';
