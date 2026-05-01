a5e8c9059266be3d1d6dde29e56aa5b1f395e021
Ademsk1 <adam.illoul24@gmail.com>
2026-05-01 11:48:14 +0100

    feat(AB#36983): adjust icon rendering logic

--- packages/angular/src/stories/Icons.stories.ts
+++ packages/angular/src/stories/Icons.stories.ts
@@ -9,36 +9,9 @@ const meta: Meta = {
 
 export default meta;
 
-const navigationMatchers = ['Arrow', 'FirstPage', 'LastPage', 'SwapVertical'];
-const socialIconNames = new Set(['Bluesky', 'Facebook', 'Instagram', 'Linkedin', 'Threads', 'Tiktok', 'X', 'Youtube']);
+const iconName = (selector: string) => selector.replace('gi-', '').replace('-icon', '');
 
-type SortedIcons = {
-  Base: typeof iconList;
-  Navigation: typeof iconList;
-  Social: typeof iconList;
-};
-
-function buildSortedIcons(): SortedIcons {
-  return iconList.reduce(
-    (acc, icon) => {
-      if (navigationMatchers.some((matcher) => icon.name.includes(matcher))) {
-        acc.Navigation.push(icon);
-      } else if (socialIconNames.has(icon.name)) {
-        acc.Social.push(icon);
-      } else {
-        acc.Base.push(icon);
-      }
-      return acc;
-    },
-    {
-      Base: [],
-      Navigation: [],
-      Social: [],
-    } as SortedIcons,
-  );
-}
-
-function iconItemTemplate({ name, selector }: (typeof iconList)[number]) {
+function iconItemTemplate({ selector }: { name: string; selector: string }) {
   return `
       <div class="gi-w-32 gi-flex gi-flex-col gi-items-center gi-gap-2">
         <${selector}
@@ -48,36 +21,32 @@ function iconItemTemplate({ name, selector }: (typeof iconList)[number]) {
           [className]="className"
           dataTestId="${selector}"
         ></${selector}>
-        <span class="gi-text-xs gi-text-center gi-whitespace-nowrap">${name}</span>
+        <span class="gi-text-xs gi-text-center gi-whitespace-nowrap">${iconName(selector)}</span>
       </div>`;
-}
-
-function sectionsTemplate(sortedIcons: SortedIcons): string {
-  return Object.entries(sortedIcons)
-    .map(
-      ([key, icons]) => `
-    <div class="gi-flex gi-flex-col gi-gap-8">
-      <h2 class="gi-text-lg gi-font-bold gi-mb-8 gi-underline gi-text-center">${key}</h2>
-      <div class="gi-flex gi-flex-wrap gi-gap-8">
-        ${_.map(icons, iconItemTemplate).join('')}
-      </div>
-    </div>`,
-    )
-    .join('');
 }
 
 export const Default: StoryObj = {
   ...iconsDefault,
   render: (props) => {
-    const sortedIcons = buildSortedIcons();
     return {
       props,
       moduleMetadata: {
-        imports: _.map(iconList, 'Component'),
+        imports: _.map(_.flatMap(Object.values(iconList)), 'Component'),
       },
       template: `
     <div class="gi-flex gi-flex-col gi-gap-8">
-      ${sectionsTemplate(sortedIcons)}
+      ${Object.entries(iconList)
+        .map(
+          ([key, icons]) => `
+        <div class="gi-flex gi-flex-col gi-gap-8">
+          <h2 class="gi-text-lg gi-font-bold gi-mb-8 gi-underline gi-text-center">${_.startCase(key)}</h2>
+          <div class="gi-flex gi-flex-wrap gi-gap-8">
+            ${_.map(icons, iconItemTemplate).join('')}
+          </div>
+        </div>
+      `,
+        )
+        .join('')}
     </div>
     `,
     };
--- packages/react/src/stories/Icons.stories.tsx
+++ packages/react/src/stories/Icons.stories.tsx
@@ -15,56 +15,19 @@ const meta: Meta = {
 
 export default meta;
 
-const navigationMatchers = ['Arrow', 'FirstPage', 'LastPage', 'SwapVertical'];
-const socialIconNames = new Set([
-  'Bluesky',
-  'Facebook',
-  'Instagram',
-  'Linkedin',
-  'Threads',
-  'Tiktok',
-  'X',
-  'Youtube',
-]);
-
-type SortedIcons = {
-  Base: typeof iconList;
-  Navigation: typeof iconList;
-  Social: typeof iconList;
-};
-
 export const Default: StoryObj = {
   ...iconsDefault,
-  render: (props: IconProps) => {
-    const sortedIcons = iconList.reduce(
-      (acc, icon) => {
-        console.log(icon.name);
-        if (navigationMatchers.some((matcher) => icon.name.includes(matcher))) {
-          acc.Navigation.push(icon);
-        } else if (socialIconNames.has(icon.name)) {
-          acc.Social.push(icon);
-        } else {
-          acc.Base.push(icon);
-        }
-        return acc;
-      },
-      {
-        Base: [],
-        Navigation: [],
-        Social: [],
-      } as SortedIcons,
-    );
-
+  render: () => {
     return (
       <div className="gi-flex gi-flex-col gi-gap-8 ">
-        {Object.entries(sortedIcons).map(([key, icons]) => (
+        {Object.entries(iconList).map(([key, icons]) => (
           <div key={key} className="gi-border-b gi-pb-4">
             <h2 className="gi-text-lg gi-font-bold gi-mb-8 gi-underline gi-text-center">
-              {key}
+              {_.startCase(key)}
             </h2>
             <div className="gi-flex gi-flex-wrap gi-gap-8">
               {_.map(icons, (icon) => (
-                <IconItem {...icon} />
+                <IconItem key={icon.selector} {...icon} />
               ))}
             </div>
           </div>
@@ -83,10 +46,7 @@ const IconItem = ({
   selector: string;
 }) => {
   return (
-    <div
-      key={selector}
-      className=" gi-w-32 gi-flex gi-flex-col gi-items-center gi-gap-2"
-    >
+    <div className=" gi-w-32 gi-flex gi-flex-col gi-items-center gi-gap-2">
       <Component size={48} dataTestId={selector} />
       <span className="gi-text-xs gi-text-center gi-whitespace-nowrap">
         {name}
