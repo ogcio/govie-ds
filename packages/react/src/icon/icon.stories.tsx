bc383f038056d43f3ae57679769c55bb85bb7e52
Ademsk1 <adam.illoul24@gmail.com>
2026-06-15 10:04:57 +0100

    feat(AB#38091): address comments

--- packages/react/src/icon/icon.stories.tsx
+++ packages/react/src/icon/icon.stories.tsx
@@ -188,39 +188,3 @@ export const TestThumbDownLarge: Story = {
     });
   },
 };
-export const EnablingMaterialSymbols: StoryObj<
-  Pick<IconProps, 'icon'> & {
-    addLink: boolean;
-  }
-> = {
-  tags: ['skip-playwright'],
-  args: { icon: 'add_circle', addLink: true },
-  parameters: {
-    controls: { include: ['addLink'] },
-    docs: {
-      description: {
-        story:
-          'By adding in a stylesheet link to the `<head>`, you may continue to use the Material Symbols font fallback. Select the story, and change the `addLink` prop to false to see what happens when the stylesheet is removed. You should ensure that all the icons you require are within the icon_names list in the `href` link. ',
-      },
-    },
-  },
-  argTypes: {
-    addLink: {
-      control: 'boolean',
-      description: 'Include the Material Symbols stylesheet link in the document.',
-    },
-  },
-  render: function Render({ icon, addLink }) {
-    return (
-      <>
-        {addLink && (
-          <link
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,400,0..1,0&icon_names=accessibility_new,add_circle,apps,arrow_back,arrow_downward,arrow_drop_down,arrow_drop_up,arrow_forward,arrow_left_alt,arrow_outward,arrow_right_alt,arrow_upward,attach_file,block,call,cancel,candlestick_chart,chat_bubble,check,check_circle,chevron_left,chevron_right,child_care,close,content_copy,credit_card,delete,directions_car,do_not_disturb_on,download,edit,error,event,filter_list,first_page,health_and_safety,home,info,keyboard_arrow_down,keyboard_arrow_up,last_page,link,location_on,login,logout,mail,menu,mic,more_horiz,more_vert,open_in_new,person,person_cancel,person_check,refresh,search,send,settings,sort,space_dashboard,swap_vert,sync,thumb_down,thumb_up,unfold_more,upload,visibility,visibility_off,warning,work"
-            rel="stylesheet"
-          />
-        )}
-        <Icon icon={icon} useFontIcon={true} />
-      </>
-    );
-  },
-};
