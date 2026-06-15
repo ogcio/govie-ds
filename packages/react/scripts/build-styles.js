9c0cf2ce1bc14e41f7e2bc297e55bcbf2ce612af
Ademsk1 <adam.illoul24@gmail.com>
2026-06-15 15:05:01 +0100

    feat(AB#38091): use css file as material symbols import

--- packages/react/scripts/build-styles.js
+++ packages/react/scripts/build-styles.js
@@ -2,5 +2,7 @@ import { readFileSync, writeFileSync } from 'node:fs';
 
 const fontsCSS = readFileSync(new URL('../fonts.css', import.meta.url), 'utf8');
 const stylesCSS = readFileSync(new URL('../dist/styles.css', import.meta.url), 'utf8');
+const materialSymbols = readFileSync(new URL('../material-symbols.css', import.meta.url), 'utf8');
 
 writeFileSync(new URL('../dist/styles.css', import.meta.url), fontsCSS + stylesCSS);
+writeFileSync(new URL('../dist/material-symbols.css', import.meta.url), materialSymbols);
