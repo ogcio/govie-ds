d7c3ef04e244fc9af79908c3e574a32938107275
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2026-06-29 09:00:16 +0100

    feat(AB#39585): add FooterLogo component (#996)

---
+++ packages/core/atoms/FooterLogo.lite.tsx
@@ -0,0 +1,30 @@
+import { useMetadata } from '@builder.io/mitosis';
+import { tv } from 'tailwind-variants';
+import GiBox from './Box.lite';
+
+useMetadata({ angular: { selector: 'gi-footer-logo' } });
+
+export type Props = {
+  children: any;
+  id?: string;
+  className?: string;
+  styles?: Record<string, string>;
+  dataTestId?: string;
+};
+
+export default function FooterLogo(props: Props) {
+  return (
+    <GiBox
+      id={props.id}
+      className={footerStyles({ class: props.className })}
+      styles={props.styles}
+      dataTestId={props.dataTestId}
+    >
+      {props.children}
+    </GiBox>
+  );
+}
+
+const footerStyles = tv({
+  base: 'gi-w-fit md:gi-ml-auto gi-mt-8 md:gi-mt-0',
+});
