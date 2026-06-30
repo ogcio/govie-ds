a8179a8bb3055979b2d7b83e80f81b431a938f44
Ademsk1 <adam.illoul24@gmail.com>
2026-06-30 16:33:59 +0100

    fix(AB#40245): apply getOrientation & adjust stories to cap ture

--- packages/angular/src/stories/Divider.stories.ts
+++ packages/angular/src/stories/Divider.stories.ts
@@ -4,6 +4,7 @@ import Divider from '@/atoms/Divider';
 import Stack from '@/atoms/Stack';
 import * as stories from '@/atoms/storybook/Divider.meta';
 import { Orientation } from '@/atoms/constants';
+import { Link } from '@/atoms';
 
 const meta = {
   ...stories.meta,
@@ -16,16 +17,16 @@ export const Horizontal: StoryObj = {
   ...stories.Horizontal,
   render: (props) => ({
     props,
-    moduleMetadata: { imports: [Box, Divider, Stack] },
+    moduleMetadata: { imports: [Box, Divider, Stack, Link] },
     template: `
       <gi-stack [gap]="2" [direction]="orientation === '${Orientation.VERTICAL}' ? 'row' : 'column'" className="gi-font-primary gi-text-sm">
-        <gi-box>Content</gi-box>
+        <gi-box><gi-link href="/" variant="inline">Above</gi-link></gi-box>
         <gi-divider
           [orientation]="orientation"
           [id]="id"
           [dataTestId]="dataTestId"
         ></gi-divider>
-        <gi-box>Content</gi-box>
+        <gi-box><gi-link href="/" variant="inline">Below</gi-link></gi-box>
       </gi-stack>
     `,
   }),
@@ -35,16 +36,16 @@ export const Vertical: StoryObj = {
   ...stories.Vertical,
   render: (props) => ({
     props,
-    moduleMetadata: { imports: [Box, Divider, Stack] },
+    moduleMetadata: { imports: [Box, Divider, Stack, Link] },
     template: `
       <gi-stack [direction]="'row'" [gap]="2" className="gi-font-primary gi-text-sm">
-        <gi-box>Left</gi-box>
+        <gi-box><gi-link href="/" variant="inline">Left</gi-link></gi-box>
         <gi-divider
           [orientation]="orientation"
           [id]="id"
           [dataTestId]="dataTestId"
         ></gi-divider>
-        <gi-box>Right</gi-box>
+        <gi-box><gi-link href="/" variant="inline">Right</gi-link></gi-box>
       </gi-stack>
     `,
   }),
